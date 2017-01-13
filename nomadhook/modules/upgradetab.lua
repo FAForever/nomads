ea061695951b9d8808f0a7eec28c52f6b5771745
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-01-13 21:09:01 +0100

    add upgrade route for nomad units
    fixes #26

---
+++ nomadhook/modules/upgradetab.lua
@@ -0,0 +1,21 @@
+upgradeTab =
+{
+    -- nomads
+    ['inb0101'] = {'inb0211', 'inb0201'},
+    ['inb0102'] = {'inb0212', 'inb0202'},
+    ['inb0103'] = {'inb0213', 'inb0203'},
+    
+    ['inb0201'] = 'inb0301',    -- HQs
+    ['inb0202'] = 'inb0302',
+    ['inb0203'] = 'inb0303',
+
+    ['inb0211']  = 'inb0311',   -- support factories
+    ['inb0212']  = 'inb0312',
+    ['inb0213']  = 'inb0313',
+    ['inb3102']  = 'inb3202',   -- sonar
+    ['inb3202']  = 'inb3302',
+    ['inb3101']  = 'inb3201',   -- radar
+    ['inb3201']  = 'inb3301',
+    ['inb1102']  = 'inb1202',   -- mex
+    ['inb1202']  = 'inb1302',
+}
