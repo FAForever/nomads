da10529c392f8c5e7bceb5f47ed70cbb34f1d353
RK <orvolraz@yahoo.com>
2017-01-26 01:08:56 +0100

    Completely redesigned ACU cannon explosion
    what it says on the tin

--- projectiles/NAPRound2/NAPRound2_script.lua
+++ projectiles/NAPRound2/NAPRound2_script.lua
@@ -1,21 +1,7 @@
 local NomadEffectTemplate = import('/lua/nomadeffecttemplate.lua')
-local APRound = import('/lua/nomadprojectiles.lua').APRound
+local APRoundCap = import('/lua/nomadprojectiles.lua').APRoundCap
+local NomadExplosions = import('/lua/nomadexplosions.lua')
 
-NAPRound2 = Class(APRound) {
-
-    FxImpactAirUnit = NomadEffectTemplate.APCannonHitAirUnit2,
-    FxImpactLand = NomadEffectTemplate.APCannonHitLand2,
-    FxImpactNone = NomadEffectTemplate.APCannonHitNone2,
-    FxImpactProp = NomadEffectTemplate.APCannonHitProp2,
-    FxImpactShield = NomadEffectTemplate.APCannonHitShield2,
-    FxImpactUnit = NomadEffectTemplate.APCannonHitUnit2,
-    FxImpactWater = NomadEffectTemplate.APCannonHitWater2,
-    FxImpactProjectile = NomadEffectTemplate.APCannonHitProjectile2,
-    FxImpactUnderWater = NomadEffectTemplate.APCannonHitUnderWater2,
-
-    FxImpactTrajectoryAligned = false,
-    FxTrails = NomadEffectTemplate.APCannonTrail2,
-    PolyTrail = NomadEffectTemplate.APCannonPolyTrail2,
-}
+NAPRound2 = Class(APRoundCap) {}
 
 TypeClass = NAPRound2
