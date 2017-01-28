56946ebe0fde3dc83fd9945f9d5220e0b7e6246d
RK <orvolraz@yahoo.com>
2017-01-28 03:21:07 +0100

    More pronounced effect for multiple projectiles
    <3 u exotic

--- lua/nomadseffecttemplate.lua
+++ lua/nomadseffecttemplate.lua
@@ -244,7 +244,7 @@ DarkMatterAirWeaponPolyTrails = {
 DarkMatterAirWeaponHitNone1 = {
     EmtBpPathNomads .. 'nomads_DarkMatterWeapon_hit_01_emit_air.bp', -- idk
     EmtBpPathNomads .. 'nomads_DarkMatterWeapon_hit_02_emit_air.bp', -- explosions
-	--EmtBpPathNomads .. 'nomads_DarkMatterWeapon_hit_03_emit_air.bp', -- idk
+	EmtBpPathNomads .. 'nomads_DarkMatterWeapon_hit_03_emit_air.bp', -- idk
 }
 
 DarkMatterAirWeaponHitLand1 = TableCat( DarkMatterAirWeaponHitNone1, {
