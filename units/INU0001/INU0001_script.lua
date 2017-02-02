481dee585a81625cdf00430f273c470bee849615
Exotic-Retard <ddoogedoge@gmail.com>
2017-02-02 19:03:53 +0000

    Update ACU to use transport animations
    should have a land/unland animation and look awesome.

--- units/INU0001/INU0001_script.lua
+++ units/INU0001/INU0001_script.lua
@@ -335,6 +335,30 @@ INU0001 = Class(ACUUnit) {
         ACUUnit.UpdateMovementEffectsOnMotionEventChange( self, new, old )
     end,
 
+    -------- TRANSPORT ANIM --------
+    OnStartTransportBeamUp = function(self, transport, bone)
+        local slot = transport.slots[bone]
+        if slot then
+            self:GetAIBrain():OnTransportFull()
+            IssueClearCommands({self})
+            return
+        end
+        self:DestroyIdleEffects()
+        self:DestroyMovementEffects()
+        local army =  self:GetArmy()
+        table.insert( self.TransportBeamEffectsBag, AttachBeamEntityToEntity(self, -1, transport, bone, army, EffectTemplate.TTransportBeam01))
+        table.insert( self.TransportBeamEffectsBag, AttachBeamEntityToEntity( transport, bone, self, -1, army, EffectTemplate.TTransportBeam02))
+        table.insert( self.TransportBeamEffectsBag, CreateEmitterAtBone( transport, bone, army, EffectTemplate.TTransportGlow01) )
+        self:TransportAnimation(1)
+    end,
+    
+    OnDetachedFromTransport = function(self, transport, bone)
+        self:MarkWeaponsOnTransport(false)
+        self:EnableShield()
+        self:EnableDefaultToggleCaps()
+        self:TransportAnimation(-0.7)
+        self:DoUnitCallbacks( 'OnDetachedFromTransport', transport, bone)
+    end,
     -- =====================================================================================================================
     -- ORBITAL ENHANCEMENTS
 
