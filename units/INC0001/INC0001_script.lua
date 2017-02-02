1cc5cf96855a32dc8a09b3a31a87868a6dd429ec
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-02-02 13:07:21 +0100

    INC0001: remove unnecessary .dds files and improve/fix (#106)
    Fixed the selection box and added the hover and rotate mechanic.
    
    EDIT: fix CollisionBox and change production/storage values
    EDIT2: Change categories so air can target it, no maa or saa though
    EDIT3: Changed elevation 4.2 and hp 20000
    EDIT4: Added economy information so repairs are not so easy

--- units/INC0001/INC0001_script.lua
+++ units/INC0001/INC0001_script.lua
@@ -8,20 +8,58 @@ INC0001 = Class(NCivilianStructureUnit) {
         self.BuildEffectsBag = TrashBag()
         NCivilianStructureUnit.OnCreate(self)
         
+        if self:GetBlueprint().Physics.Elevation then
+            self:Hover()
+        end
+        if self:GetBlueprint().Rotators.Stationary then
+            self:StationaryAngle()
+        else
+            self:RotatingAngle()
+        end
+    end,
+    
+    Hover = function(self)
+        local pos = self:GetPosition()
+        local surface = GetSurfaceHeight(pos[1], pos[3]) + GetTerrainTypeOffset(pos[1], pos[3])
+        local elevation = self:GetBlueprint().Physics.Elevation
+        pos[2] = surface + elevation
+        self:SetPosition( pos, true)
+    end,
+    
+    RotatingAngle = function(self)
         -- spinner 1
         if not self.RotatorManipulator1 then
             self.RotatorManipulator1 = CreateRotator( self, 'Primary_Spinner', 'z' )
-            self.RotatorManipulator1:SetCurrentAngle( 45 )
+            self.RotatorManipulator1:SetAccel( self:GetBlueprint().Rotators.PrimaryAccel ) 
+            self.RotatorManipulator1:SetTargetSpeed( self:GetBlueprint().Rotators.PrimarySpeed ) 
             self.Trash:Add( self.RotatorManipulator1 )
         end
 
         -- spinner 2
         if not self.RotatorManipulator2 then
             self.RotatorManipulator2 = CreateRotator( self, 'Secondary_Spinner', 'z' )
-            self.RotatorManipulator2:SetCurrentAngle( -45 )
+            self.RotatorManipulator2:SetAccel( self:GetBlueprint().Rotators.SecondaryAccel ) 
+            self.RotatorManipulator2:SetTargetSpeed( self:GetBlueprint().Rotators.SecondarySpeed ) 
+            self.Trash:Add( self.RotatorManipulator2 )
+        end
+        
+    end,
+    
+    StationaryAngle = function(self)
+        -- spinner 1
+        if not self.RotatorManipulator1 then
+            self.RotatorManipulator1 = CreateRotator( self, 'Primary_Spinner', 'z' )
+            self.RotatorManipulator1:SetCurrentAngle( self:GetBlueprint().Rotators.PrimaryAngle )
+            self.Trash:Add( self.RotatorManipulator1 )
+        end
+
+        -- spinner 2
+        if not self.RotatorManipulator2 then
+            self.RotatorManipulator2 = CreateRotator( self, 'Secondary_Spinner', 'z' )
+            self.RotatorManipulator2:SetCurrentAngle( self:GetBlueprint().Rotators.SecondaryAngle )
             self.Trash:Add( self.RotatorManipulator2 )
         end
     end,
-    }
+}
 
 TypeClass = INC0001
