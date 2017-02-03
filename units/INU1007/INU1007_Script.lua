25b8ae59726d030534682239e5096c60127a42f0
JJ173 <jack.t371@hotmail.co.uk>
2017-02-03 15:52:07 +0000

    Fix OnWaterBuff Error for Hover Units (Light Tank)
    Removes OnWaterBuff Error and adds code to slow unit down on water.

--- units/INU1007/INU1007_Script.lua
+++ units/INU1007/INU1007_Script.lua
@@ -2,6 +2,7 @@
 
 local NHoverLandUnit = import('/lua/nomadsunits.lua').NHoverLandUnit
 local DarkMatterWeapon1 = import('/lua/nomadsweapons.lua').DarkMatterWeapon1
+local SlowHover = import('/lua/defaultunits.lua').SlowHoverLandUnit
 
 INU1007 = Class(NHoverLandUnit) {
     Weapons = {
