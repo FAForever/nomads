4f8068f14dcaf717bc7e54c5a786b1999004326d
JJ173 <jack.t371@hotmail.co.uk>
2017-02-03 23:57:44 +0000

    Update nomadsunits.lua

--- lua/nomadsunits.lua
+++ lua/nomadsunits.lua
@@ -340,23 +340,6 @@ NExperimentalHoverLandUnit = Class(NHoverLandUnit) {
         DamageArea(self, pos, bpDamage.Radius, bpDamage.Amount, bpDamage.Type, bpDamage.DamageFriendly )
     end,
 }
-
-SlowHoverLandUnit = Class(HoverLandUnit) {
-    OnLayerChange = function(self, new, old)
-        HoverLandUnit.OnLayerChange(self, new, old)
-        -- Slow these units down when they transition from land to water
-        -- The mult is applied twice thanks to an engine bug, so careful when adjusting it
-        -- Newspeed = oldspeed * mult * mult
-
-        local mult = self:GetBlueprint().Physics.WaterSpeedMultiplier
-        if new == 'Water' then
-            self:SetSpeedMult(mult)
-        else
-            self:SetSpeedMult(1)
-        end
-    end,
-}
-
 ---------------------------------------------------------------
 --  SEA UNITS
 ---------------------------------------------------------------
