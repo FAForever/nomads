0c10095050d5672ef5a9b1c0a02368b2bcee4fa2
JJ173 <jack.t371@hotmail.co.uk>
2017-02-03 15:48:29 +0000

    Fix OnWaterBuff Error for Hover Units (Medium Tank)
    Fixed to stop error if unit is created on Water and slows the unit down.

--- units/INU1004/INU1004_Script.lua
+++ units/INU1004/INU1004_Script.lua
@@ -2,6 +2,7 @@
 
 local NHoverLandUnit = import('/lua/nomadsunits.lua').NHoverLandUnit
 local ParticleBlaster1 = import('/lua/nomadsweapons.lua').ParticleBlaster1
+local SlowHover = import('/lua/defaultunits.lua').SlowHoverLandUnit
 
 INU1004 = Class(NHoverLandUnit) {
     Weapons = {
