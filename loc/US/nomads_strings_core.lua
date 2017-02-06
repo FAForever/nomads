3fb633ef82f88df83475ad08837c4f1b854b5186
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-02-06 20:52:48 +0100

    fix/ remove wrong LOC entries which screw up the factory names

--- loc/US/nomads_strings_core.lua
+++ loc/US/nomads_strings_core.lua
@@ -549,50 +549,35 @@ ins2003_name = "Whaler Class"
 
 -- Structures
 
--- land factory
-inb0201_desc = "Land Factory"
-inb0201_help = "Constructs Tech 2 land units. Upgradeable."
-inb0201_name = "Land Factory"
-
 -- land factory (HQ, FAF specific)
-inb0201faf_desc = "Land Factory (HQ)"
-inb0201faf_help = "Constructs Tech 2 land units. HQ factory, required to build same tier land units and same tier land support factories. Upgradeable."
-inb0201faf_name = "Land Factory (HQ)"
+inb0201_desc = "Land Factory (HQ)"
+inb0201_help = "Constructs Tech 2 land units. HQ factory, required to build same tier land units and same tier land support factories. Upgradeable."
+inb0201_name = "Land Factory (HQ)"
 
 -- land factory (support, FAF specific)
-inb0211faf_desc = "Land Factory (Support)"
-inb0211faf_help = "Constructs Tech 2 land units. Supporting factory, requires a land HQ factory to build same tier land units. Upgradeable once a land HQ factory is built."
-inb0211faf_name = "Land Factory (support)"
-
--- air factory
-inb0202_desc = "Air Factory"
-inb0202_help = "Constructs Tech 2 air units. Upgradeable."
-inb0202_name = "Air Factory"
+inb0211_desc = "Land Factory (Support)"
+inb0211_help = "Constructs Tech 2 land units. Supporting factory, requires a land HQ factory to build same tier land units. Upgradeable once a land HQ factory is built."
+inb0211_name = "Land Factory (support)"
 
 -- air factory (HQ, FAF specific)
-inb0202faf_desc = "Air Factory (HQ)"
-inb0202faf_help = "Constructs Tech 2 air units. HQ factory, required to build same tier air units and same tier air support factories. Upgradeable."
-inb0202faf_name = "Air Factory (HQ)"
+inb0202_desc = "Air Factory (HQ)"
+inb0202_help = "Constructs Tech 2 air units. HQ factory, required to build same tier air units and same tier air support factories. Upgradeable."
+inb0202_name = "Air Factory (HQ)"
 
 -- air factory (support, FAF specific)
-inb0212faf_desc = "Air Factory (Support)"
-inb0212faf_help = "Constructs Tech 2 air units. Supporting factory, requires an air HQ factory to build same tier air units. Upgradeable once an air HQ factory is built."
-inb0212faf_name = "Air Factory (Support)"
-
--- naval factory
-inb0203_desc = "Naval Factory"
-inb0203_help = "Constructs Tech 2 naval units. Upgradeable."
-inb0203_name = "Naval Factory"
+inb0212_desc = "Air Factory (Support)"
+inb0212_help = "Constructs Tech 2 air units. Supporting factory, requires an air HQ factory to build same tier air units. Upgradeable once an air HQ factory is built."
+inb0212_name = "Air Factory (Support)"
 
 -- naval factory (HQ, FAF specific)
-inb0203faf_desc = "Naval Factory (HQ)"
-inb0203faf_help = "Constructs Tech 2 naval units. HQ factory, required to build same tier naval units and same tier naval support factories. Upgradeable."
-inb0203faf_name = "Naval Factory (HQ)"
+inb0203_desc = "Naval Factory (HQ)"
+inb0203_help = "Constructs Tech 2 naval units. HQ factory, required to build same tier naval units and same tier naval support factories. Upgradeable."
+inb0203_name = "Naval Factory (HQ)"
 
 -- naval factory (support, FAF specific)
-inb0213faf_desc = "Naval Factory (Support)"
-inb0213faf_help = "Constructs Tech 2 naval units. Supporting factory, requires a naval HQ factory to build same tier naval units. Upgradeable once a naval HQ factory is built."
-inb0213faf_name = "Naval Factory (Support)"
+inb0213_desc = "Naval Factory (Support)"
+inb0213_help = "Constructs Tech 2 naval units. Supporting factory, requires a naval HQ factory to build same tier naval units. Upgradeable once a naval HQ factory is built."
+inb0213_name = "Naval Factory (Support)"
 
 -- Power generator
 inb1201_desc = "Antimatter-Reactor"
