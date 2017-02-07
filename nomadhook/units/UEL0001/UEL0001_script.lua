76089f7e9f892c0a0ca81ed40e4a3e3a2e1dd3e8
JJ173 <jack.t371@hotmail.co.uk>
2017-02-07 13:19:04 +0000

    Added FailSafe for Billy Upgrade (Removes Button)

--- nomadhook/units/UEL0001/UEL0001_script.lua
+++ nomadhook/units/UEL0001/UEL0001_script.lua
@@ -7,6 +7,9 @@ UEL0001 = Class(oldUEL0001) {
 	if enh =='TacticalMissile' then
 		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = false
 		self:RegisterSpecialAbilities()
+	elseif enh = 'TacticalNukeMissile' then
+    		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = true
+    		self:UnregisterSpecialAbilities()
 	elseif enh == 'TacticalMissileRemove' then
 		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = true
 		self:UnregisterSpecialAbilities()
