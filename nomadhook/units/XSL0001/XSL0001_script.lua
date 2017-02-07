e2833ab54e4bfd9159f109e8dfc589682efc90eb
JJ173 <jack.t371@hotmail.co.uk>
2017-02-07 11:14:35 +0000

    Removed LOG from Seraphim ACU Script.

--- nomadhook/units/XSL0001/XSL0001_script.lua
+++ nomadhook/units/XSL0001/XSL0001_script.lua
@@ -4,14 +4,12 @@ XSL0001 = Class(oldXSL0001) {
 	CreateEnhancement = function(self, enh)
 		oldXSL0001.CreateEnhancement(self, enh)
 		
-		if enh =='Missile' then
-			LOG('Missile Attached')
-			self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = false
-			self:RegisterSpecialAbilities()
-		elseif enh == 'MissileRemove' then
-			LOG('Missile Removed')
-			self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = true
-			self:UnregisterSpecialAbilities()
+	if enh =='Missile' then
+		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = false
+		self:RegisterSpecialAbilities()
+	elseif enh == 'MissileRemove' then
+		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = true
+		self:UnregisterSpecialAbilities()
         end
     end
 }
