1c1a7d0fc36ceb2d1317f8e5bf3d488655a44ac3
JJ173 <jack.t371@hotmail.co.uk>
2017-02-07 11:12:38 +0000

    Removed LOG from UEF ACU Script.

--- nomadhook/units/UEL0001/UEL0001_script.lua
+++ nomadhook/units/UEL0001/UEL0001_script.lua
@@ -4,17 +4,14 @@ UEL0001 = Class(oldUEL0001) {
 	CreateEnhancement = function(self, enh)
 		oldUEL0001.CreateEnhancement(self, enh)
 
-		if enh =='TacticalMissile' then
-			LOG('Tac Missile Attached')
-			self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = false
-			self:RegisterSpecialAbilities()
+	if enh =='TacticalMissile' then
+		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = false
+		self:RegisterSpecialAbilities()
         elseif enh == 'TacticalNukeMissile' then
-			LOG('Billy Attached')
-			self:RegisterSpecialAbilities()
-		elseif enh == 'TacticalMissileRemove' or enh == 'TacticalNukeMissileRemove' then
-			LOG('Tac or Billy Removed')
-			self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = true
-			self:UnregisterSpecialAbilities()
+		self:RegisterSpecialAbilities()
+	elseif enh == 'TacticalMissileRemove' or enh == 'TacticalNukeMissileRemove' then
+		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = true
+		self:UnregisterSpecialAbilities()
         end
     end
 }
