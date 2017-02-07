1c436aac676b0f4d946162e0742ca29507a876f4
JJ173 <jack.t371@hotmail.co.uk>
2017-02-07 16:21:27 +0000

    ACU "Easy Tac" button error fixed. (#138)
    * Fixed button error for "Easy Tac" button
    
    Billy Nuke will not be handled with the tml UI
    
    This PR fixes #121

---
+++ nomadhook/units/XSL0001/XSL0001_script.lua
@@ -0,0 +1,17 @@
+local oldXSL0001 = XSL0001
+
+XSL0001 = Class(oldXSL0001) {
+	CreateEnhancement = function(self, enh)
+		oldXSL0001.CreateEnhancement(self, enh)
+		
+	if enh =='Missile' then
+		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = false
+		self:RegisterSpecialAbilities()
+	elseif enh == 'MissileRemove' then
+		self:GetBlueprint().SpecialAbilities.LaunchTacMissile.NoAutoEnable = true
+		self:UnregisterSpecialAbilities()
+        end
+    end
+}
+
+TypeClass = XSL0001
