2de6deefea50f780d142b4adec94a04595f29132
Exotic-Retard <ddoogedoge@gmail.com>
2017-02-25 22:15:15 +0000

    Update soundbank and references
    this is an attempt to make it work but it crashes the game instead xD
    
    yyyyeah.

--- nomadhook/lua/ui/game/taunt.lua
+++ nomadhook/lua/ui/game/taunt.lua
@@ -4,16 +4,16 @@
 
 taunts = table.cat( taunts, {
 
-    {text = '<LOC NTaunts_MP1_010_010>[{i Nicols}]: You will never escape, I will make you suffer!', bank = 'NTaunts', cue = 'NTaunts_Nicols_0001'},
-    {text = '<LOC NTaunts_MP1_010_011>[{i Nicols}]: Your resistance is futile, you will be destroyed!', bank = 'NTaunts', cue = 'NTaunts_Nicols_0002'},
-    {text = '<LOC NTaunts_MP1_010_012>[{i Nicols}]: Orbital strike initiated, goodbye.', bank = 'NTaunts', cue = 'NTaunts_Nicols_0003'},
-    {text = '<LOC NTaunts_MP1_010_013>[{i Nicols}]: We are back. It\'s time for you to die.', bank = 'NTaunts', cue = 'NTaunts_Nicols_0004'},
-    {text = '<LOC NTaunts_MP1_010_014>[{i Nicols}]: Once we were lost, but now we will reign supreme.', bank = 'NTaunts', cue = 'NTaunts_Nicols_0005'},
-    {text = '<LOC NTaunts_MP1_010_015>[{i Nicols}]: You have what we call a very terrestrial mindset.', bank = 'NTaunts', cue = 'NTaunts_Nicols_0006'},
-    {text = '<LOC NTaunts_MP1_010_016>[{i Nicols}]: I think the space between your ears is best left unexplored!', bank = 'NTaunts', cue = 'NTaunts_Nicols_0007'},
-    {text = '<LOC NTaunts_MP1_010_017>[{i Nicols}]: You can\'t hope to contain the vastness of the nomad fleet.', bank = 'NTaunts', cue = 'NTaunts_Nicols_0008'},
-    {text = '<LOC NTaunts_MP1_010_018>[{i Nicols}]: I\'m afraid, we\'re out of mercy today. How would you like an ass kicking instead?', bank = 'NTaunts', cue = 'NTaunts_Nicols_0009'},
-    {text = '<LOC NTaunts_MP1_010_019>[{i Nicols}]: It is amazing that you made it this far without hurting yourself.', bank = 'NTaunts', cue = 'NTaunts_Nicols_0010'},
+    {text = '<LOC NTaunts_MP1_010_010>[{i Nicols}]: You will never escape, I will make you suffer!', bank = 'NTaunts', cue = 'NTaunt_Nicols_001'},
+    {text = '<LOC NTaunts_MP1_010_011>[{i Nicols}]: Your resistance is futile, you will be destroyed!', bank = 'NTaunts', cue = 'NTaunt_Nicols_002'},
+    {text = '<LOC NTaunts_MP1_010_012>[{i Nicols}]: Orbital strike initiated, goodbye.', bank = 'NTaunts', cue = 'NTaunt_Nicols_003'},
+    {text = '<LOC NTaunts_MP1_010_013>[{i Nicols}]: We are back. It\'s time for you to die.', bank = 'NTaunts', cue = 'NTaunt_Nicols_004'},
+    {text = '<LOC NTaunts_MP1_010_014>[{i Nicols}]: Once we were lost, but now we will reign supreme.', bank = 'NTaunts', cue = 'NTaunt_Nicols_005'},
+    {text = '<LOC NTaunts_MP1_010_015>[{i Nicols}]: You have what we call a very terrestrial mindset.', bank = 'NTaunts', cue = 'NTaunt_Nicols_006'},
+    {text = '<LOC NTaunts_MP1_010_016>[{i Nicols}]: I think the space between your ears is best left unexplored!', bank = 'NTaunts', cue = 'NTaunt_Nicols_007'},
+    {text = '<LOC NTaunts_MP1_010_017>[{i Nicols}]: You can\'t hope to contain the vastness of the nomad fleet.', bank = 'NTaunts', cue = 'NTaunt_Nicols_008'},
+    {text = '<LOC NTaunts_MP1_010_018>[{i Nicols}]: I\'m afraid, we\'re out of mercy today. How would you like an ass kicking instead?', bank = 'NTaunt', cue = 'NTaunt_Nicols_009'},
+    {text = '<LOC NTaunts_MP1_010_019>[{i Nicols}]: It is amazing that you made it this far without hurting yourself.', bank = 'NTaunts', cue = 'NTaunt_Nicols_010'},
 
 } )
 
