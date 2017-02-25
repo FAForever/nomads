0b2d37f6ccc845fad5a7a901288965b2a9272b5c
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-02-25 20:35:38 +0100

    enh/ orbital frigate movement stuff
    make orbital frigate fly offmap after the start
    
    it will return to the starting location when one of the upgrades that
    require it is build
    
    it will move offmap again when all upgrades that require it are removed

--- units/INO0001/INO0001_script.lua
+++ units/INO0001/INO0001_script.lua
@@ -3,6 +3,7 @@
 local NOrbitUnit = import('/lua/nomadsunits.lua').NOrbitUnit
 local OrbitalMissileWeapon = import('/lua/nomadsweapons.lua').OrbitalMissileWeapon
 local CreateNomadsBuildSliceBeams = import('/lua/nomadseffectutilities.lua').CreateNomadsBuildSliceBeams
+local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 
 INO0001 = Class(NOrbitUnit) {
     Weapons = {
@@ -21,7 +22,9 @@ INO0001 = Class(NOrbitUnit) {
     ConstructionArmAnimManip = nil,
     BuildBones = { 0, },
     BuildEffectsBag = nil,
-
+    ThrusterEffectsBag = nil,
+    returnposition = nil,
+    
     OnPreCreate = function(self)
         --yes i know this is disgusting but it has to be done since the nomads orbital ship crashes the game
         --so it needs an exception FIXME: refactor nomads orbital frigate so its not so crazy.
@@ -33,10 +36,11 @@ INO0001 = Class(NOrbitUnit) {
     
     OnCreate = function(self)
         self.BuildEffectsBag = TrashBag()
+        self.ThrusterEffectsBag = TrashBag()
+        
         NOrbitUnit.OnCreate(self)
 
         self:CreateSpinners()
-		
         local bp = self:GetBlueprint()
         if bp.Display.AnimationBuildArm then
             self.ConstructionArmAnimManip = CreateAnimator( self ):PlayAnim( bp.Display.AnimationBuildArm ):SetRate(0)
@@ -44,6 +48,8 @@ INO0001 = Class(NOrbitUnit) {
 
         self.OrbitalStrikeCurWepTarget = {}
         self.OrbitalStrikeDbKey = {}
+        self.returnposition = Vector(self:GetPosition()[1], self:GetPosition()[2], self:GetPosition()[3])
+        self.MoveAway(self)
     end,
 
     CreateSpinners = function(self)
@@ -251,6 +257,115 @@ INO0001 = Class(NOrbitUnit) {
         fz = bpP.Z + pz
         return spin, fx, fy, fz
     end,
+    
+    
+-- engines
+    ThrusterBurnBones = {'Exhaust_Centre', 'Exhaust_Top', 'Exhaust_Bottom'},
+
+    BurnEngines = function(self)
+        local army, emit = self:GetArmy()
+        local ThrusterEffects = {
+            '/effects/emitters/nomads_orbital_frigate_thruster04_emit.bp',
+            '/effects/emitters/nomads_orbital_frigate_thruster05_emit.bp',
+            '/effects/emitters/nomads_orbital_frigate_thruster01_emit.bp',
+            '/effects/emitters/nomads_orbital_frigate_thruster02_emit.bp',
+        }
+        ForkThread( function()
+            for i = 1, 4 do
+                for _, bone in self.ThrusterBurnBones do
+                    emit = CreateAttachedEmitter( self, bone, army, ThrusterEffects[i] )
+                    self.ThrusterEffectsBag:Add( emit )
+                    self.Trash:Add( emit )
+                    WaitSeconds(0.2)
+                end
+                WaitSeconds(1.5)
+            end
+        end)
+    end,
+
+    StopEngines = function(self)
+        self.ThrusterEffectsBag:Destroy()
+        local army, emit = self:GetArmy()
+        local ThrusterEffects = {
+            '/effects/emitters/nomads_orbital_frigate_thruster03_emit.bp',
+            '/effects/emitters/nomads_orbital_frigate_thruster04_emit.bp',
+        }
+        ForkThread( function()
+            for i = 1, 2 do
+                for _, bone in self.ThrusterBurnBones do
+                    emit = CreateAttachedEmitter( self, bone, army, ThrusterEffects[i] )
+                    self.ThrusterEffectsBag:Add( emit )
+                    self.Trash:Add( emit )
+                    WaitSeconds(0.2)
+                end
+            end
+            WaitSeconds(1)
+            self.ThrusterEffectsBag:Destroy()
+            for _, bone in self.ThrusterBurnBones do
+                emit = CreateAttachedEmitter( self, bone, army, ThrusterEffects[2] )
+                self.ThrusterEffectsBag:Add( emit )
+                self.Trash:Add( emit )
+            end
+            WaitSeconds(5)
+            self.ThrusterEffectsBag:Destroy()
+        end)
+    end,    
+
+
+
+-- movement behavior
+
+    MoveAway = function(self)        
+        local positionX, positionZ, positionY = unpack(self:GetPosition())
+        local mapsizeX, mapsizeY = GetMapSize()        
+        local distanceX = mapsizeX/2 - positionX
+        local distanceY = mapsizeY/2 - positionY
+        
+        local targetCoordinates
+        
+        if math.abs(distanceX) < math.abs(distanceY) then
+            if distanceY < 0 then 
+                targetCoordinates = Vector(positionX + Random(mapsizeX/5)-mapsizeX/10, positionZ, mapsizeY - 1)
+            else 
+                targetCoordinates = Vector(positionX + Random(mapsizeX/5)-mapsizeX/10, positionZ, 1)
+            end
+        else
+            if distanceX < 0 then 
+                targetCoordinates = Vector(mapsizeX - 1, positionZ, positionY + Random(mapsizeY/5)-mapsizeY/10)
+            else 
+                targetCoordinates = Vector(1, positionZ, positionY + Random(mapsizeY/5)-mapsizeY/10) 
+            end
+        end
+        
+        self.MoveCommand = IssueMove({self}, targetCoordinates)
+        self:BurnEngines()
+        
+        self:CheckIfAtTarget(targetCoordinates)
+    end,
+    
+    ReturnToStartLocation = function(self)
+        if self:GetPosition() == self.returnposition then return end
+        self.MoveCommand = IssueMove({self}, self.returnposition)
+        self:BurnEngines()
+        self:CheckIfAtTarget(self.returnposition)
+    end,
+    
+    CheckIfAtTarget = function(self, targetCoordinates)
+        ForkThread(function()
+            local arrivedAtTarget = false
+            WaitSeconds(10)
+
+            while not arrivedAtTarget do
+                if (self:GetPosition()[1]-targetCoordinates[1])^2 + (self:GetPosition()[3]-targetCoordinates[3])^2 < 10 then
+                    arrivedAtTarget = true
+                    self:StopEngines()
+                    self.MoveCommand = nil
+                end
+                WaitSeconds(2)
+            end
+        end)
+    end,
+    
 }
 
 TypeClass = INO0001
