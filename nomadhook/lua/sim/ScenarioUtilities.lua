157d744d26221aa42ae5d88ce6965f96c976e731
TheKeyBlue <keyblue357@hotmail.com>
2017-03-15 02:15:35 +0100

    AI: nomads templates fixed (temp)
    Fix nomads unit conversion by using a conversionTable which contains all
    the nomads units corresponding to uef units.
    This is a lot of work and people can just add units allong the way if
    something doesn't work properly.
    +
    Make BuildingTemplates into a hook that works

--- nomadhook/lua/sim/ScenarioUtilities.lua
+++ nomadhook/lua/sim/ScenarioUtilities.lua
@@ -1,3 +1,17 @@
+local conversionTable = {
+    uea0305 = 'ina3006',
+    uea0101 = 'ina1001',
+    uea0302 = 'ina3001',
+    uel0106 = 'inu1007',
+    uel0105 = 'inu1001',
+    uel0101 = 'inu1002',
+    uel0208 = 'inu1005',
+    uel0201 = 'inu1004',
+    uel0309 = 'inu2001',
+    uel0001 = 'inu0001',
+    uel0301 = 'inu0301',
+}
+
 local oldFactionConvert = FactionConvert
 
 function FactionConvert(template, factionIndex)
@@ -5,22 +19,16 @@ function FactionConvert(template, factionIndex)
     local i = 3
     while i <= table.getn(template) do
         if factionIndex == 5 then
-            if template[i][1] == 'uea0305' then
-                template[i][1] = 'ina3006'
-            elseif template[i][1] == 'uel0106' then
-                template[i][1] = 'inu1007'
-            elseif template[i][1] == 'uel0105' then
-                template[i][1] = 'inu1001'
-            elseif template[i][1] == 'uel0208' then
-                template[i][1] = 'inu2001'
-            elseif template[i][1] == 'uel0101' then
-                template[i][1] = 'inu1002'
+            if conversionTable[template[i][1]] then
+                template[i][1] = conversionTable[template[i][1]]
             else
+                local old = template[i][1]
                 template[i][1] = string.gsub(template[i][1], 'ue', 'in')
+                template[i][1] = string.gsub(template[i][1], 'inl', 'inu')
+                WARN('Coulndt find a conversion so we tried : ' .. old .. ' => ' .. template[i][1])
             end
         end
         i = i + 1
     end
     return template
 end
-
