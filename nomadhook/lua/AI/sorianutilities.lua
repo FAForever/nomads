3853033205bd17712d6a5be6675f2b67328f9fee
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-04-27 00:47:50 +0200

    hook GetEngineerFaction for sorian AI

--- nomadhook/lua/AI/sorianutilities.lua
+++ nomadhook/lua/AI/sorianutilities.lua
@@ -6,4 +6,19 @@ local NomadsTaunts = { 65, }
 
 AITaunts = table.insert( AITaunts, NomadsTaunts )
 
+local oldGetEngineerFaction = GetEngineerFaction
+
+function GetEngineerFaction(engineer)
+    local engyfaction =  oldGetEngineerFaction(engineer)
+    if engyfaction == false then
+        if EntityCategoryContains(categories.NOMADS, engineer) then
+            return 'Nomads'
+        else
+            return false
+        end
+    else
+        return engyfaction
+    end
+end
+
 end
