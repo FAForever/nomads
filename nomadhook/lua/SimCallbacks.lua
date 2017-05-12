9c6f7b5e0825dc9d13e84f56d0ed35a801fe438f
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-05-12 10:47:53 +0200

    remove logging from capacitor ability

--- nomadhook/lua/SimCallbacks.lua
+++ nomadhook/lua/SimCallbacks.lua
@@ -2,29 +2,23 @@ LetterArray["Nomads"] = "in"
 
 Callbacks.ActivateCapacitor = function(data, units)
     for _, u in units or {} do
-        WARN("ActivateCapacitor")
         if IsEntity(u) and OkayToMessWithArmy(u:GetArmy()) and u.Sync.HasCapacitorAbility then
-            WARN("HasCapacitorAbility")
             --active
             if u:IsCapActive() then
-                WARN("IsCapActive")
                 continue
             end
             --charged
             if u:IsCapCharged() then
-                WARN("IsCharged")
                 u:TryToActivateCapacitor()
                 continue
             end
             --charging
             if u:IsCapCharging() then
-                WARN("IsCharging")
                 u:PauseCharging()
                 continue
             end
             --paused
             if u:IsCapPaused() then
-                WARN("IsPaused")
                 u:StartCharging()
                 continue
             end
@@ -33,7 +27,6 @@ Callbacks.ActivateCapacitor = function(data, units)
 end
 Callbacks.AutoCapacitor = function(data, units)
     for _, u in units or {} do
-        WARN("AutoCapacitor")
         if IsEntity(u) and OkayToMessWithArmy(u:GetArmy()) and u.SetAutoCapacitor then
             u:SetAutoCapacitor(data.auto == true)
         end
