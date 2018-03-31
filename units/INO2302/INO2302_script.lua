fafa2da67fe483582195cc0818c6eb6bd3a71349
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2018-03-31 16:19:01 +0200

    make arty logic simple by disconnecting sat

--- units/INO2302/INO2302_script.lua
+++ units/INO2302/INO2302_script.lua
@@ -6,29 +6,6 @@ local OrbitalGun = import('/lua/nomadsweapons.lua').OrbitalGun
 INO2302 = Class(NOrbitUnit) {
     Weapons = {
         MainGun = Class(OrbitalGun) {
-
-            MasterWeaponBlueprint = function(self, _mbp)
-                self.mbp = {}
-                self.mbp['DamageAmount'] = _mbp.Damage
-                self.mbp['DamageRadius'] = _mbp.DamageRadius
-                self.mbp['DamageType'] = _mbp.DamageType
-                self.mbp['DamageFriendly'] = _mbp.DamageFriendly
-                self.mbp['DoTTime'] = _mbp.DoTTime
-                self.mbp['DoTPulses'] = _mbp.DoTPulses
-            end,
-
-            GetDamageTable = function(self)
-                local damageTable = OrbitalGun.GetDamageTable(self)
-                for k, v in self.mbp do
-                    damageTable[k] = v
-                end
-                return damageTable
-            end,
-
-            OnWeaponFired = function(self)
-                OrbitalGun.OnWeaponFired(self)
-                self.unit:OnWeaponFired()
-            end,
         },
     },
 
@@ -36,7 +13,6 @@ INO2302 = Class(NOrbitUnit) {
 
     OnCreate = function(self)
         NOrbitUnit.OnCreate(self)
-
 -- TODO: fix this. Gives weird behaviour right now
 --        -- create the engine thrust manipulators and set up the thursting arcs for the engines
 --        self.EngineManipulators = {}
@@ -47,39 +23,17 @@ INO2302 = Class(NOrbitUnit) {
 --            self.Trash:Add(manip)
 --            table.insert(self.EngineManipulators, manip)
 --        end
-
         self:SetWeaponEnabledByLabel('MainGun', false)
-
+        self:SetUnSelectable(true)
+        
         self.parent = nil
         self.parentCallbacks = {}
-        self.parentCallbacks[ 'OnWeaponFired' ] = false
         self.parentCallbacks[ 'OnKilledUnit' ] = false
     end,
 
-    OnSetParent = function(self, parent, cbWepFired, cbKilledUnit)
+    OnSetParent = function(self, parent, cbKilledUnit)
         self.parent = parent
-        self.parentCallbacks[ 'OnWeaponFired' ] = cbWepFired or false
         self.parentCallbacks[ 'OnKilledUnit' ] = cbKilledUnit or false
-
-        -- set my gun equal to what the parent has set in terms of range and damage, etc. This way we can balance the artillery
-        -- by adjusting the base unit only.
-        local TheirGun = parent:GetWeaponByLabel('TargetFinder')
-        if TheirGun then
-            local gbp = TheirGun:GetBlueprint()
-            local MyGun = self:GetWeaponByLabel('MainGun')
-            MyGun:ChangeMaxRadius( gbp.MaxRadius or 1)
-            MyGun:ChangeMinRadius( gbp.MinRadius or 0)
-            MyGun:ChangeRateOfFire ( gbp.RateOfFire or 1)
-            MyGun:SetFiringRandomness( gbp.FiringRandomness or 0)
-            MyGun:MasterWeaponBlueprint( gbp)
-        end
-    end,
-
-    OnWeaponFired = function(self)
-        local cb = self.parentCallbacks[ 'OnWeaponFired' ]
-        if cb then
-            cb( self.parent )
-        end
     end,
 
     OnKilledUnit = function(self, unitKilled)
@@ -98,18 +52,6 @@ INO2302 = Class(NOrbitUnit) {
             self:AddXP(-self.xp)
         end
         -- TODO: maybe some effects stop? lights, I dont know..
-    end,
-
-    SetTarget = function( self, target, targetPos )
-        local MyGun = self:GetWeapon(1)
-        if target and target ~= nil then
-            MyGun:SetTargetEntity( target )
-        elseif targetPos and targetPos ~= nil then
-            MyGun:SetTargetGround( targetPos )
-        else
-            MyGun:OnLostTarget()
-            MyGun:ResetTarget()
-        end
     end,
 
     EnableWeapon = function(self, enable)
