3e63f2d5f709be8e925370551311c5c6f61e9362
Exotic-Retard <ddoogedoge@gmail.com>
2018-07-18 21:02:40 +0100

    Add animation to T3 ASF
    the nomads asf now unpacks its wings when fighting, how cute!
    
    yeah pretty much this.

--- units/INA3003/INA3003_Script.lua
+++ units/INA3003/INA3003_Script.lua
@@ -5,9 +5,26 @@ local AirToAirGun1 = import('/lua/nomadsweapons.lua').AirToAirGun1
 
 INA3003 = Class(NAirUnit) {
     Weapons = {
-        RightGun = Class(AirToAirGun1) {},
-        LeftGun = Class(AirToAirGun1) {},
+        MainGun = Class(AirToAirGun1) {
+            OnStartTracking = function(self, label)
+                AirToAirGun1.OnStartTracking(self, label)
+                self.unit.animator:SetRate(0.2)
+            end,
+        
+            OnStopTracking = function(self, label)
+                AirToAirGun1.OnStopTracking(self, label)
+                self.unit.animator:SetRate(-0.2)
+            end,
+        },
     },
+
+    OnCreate = function(self)
+        NAirUnit.OnCreate(self)
+        local bp = self:GetBlueprint()
+        self.animator = CreateAnimator(self)
+        self.Trash:Add(self.animator)
+        self.animator:PlayAnim(bp.Display.AnimationOpen, false):SetRate(0)
+    end,
 }
 
 TypeClass = INA3003
