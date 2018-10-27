00ead0142638d96c8375408e102c7001557436c1
Apofenas <35565415+Apofenas@users.noreply.github.com>
2018-10-27 16:58:54 +0900

    Bombard remove

--- units/INU3003/INU3003_Script.lua
+++ units/INU3003/INU3003_Script.lua
@@ -1,10 +1,8 @@
 -- T2 EMP tank
 
-local AddBombardModeToUnit = import('/lua/nomadsutils.lua').AddBombardModeToUnit
 local NLandUnit = import('/lua/nomadsunits.lua').NLandUnit
 local EMPGun = import('/lua/nomadsweapons.lua').EMPGun
 
-NLandUnit = AddBombardModeToUnit( NLandUnit )
 
 INU3003 = Class(NLandUnit) {
     Weapons = {
@@ -12,25 +10,6 @@ INU3003 = Class(NLandUnit) {
             FxMuzzleFlash = import('/lua/nomadseffecttemplate.lua').EMPGunMuzzleFlash_Tank,
         },
     },
-
-    SetBombardmentMode = function(self, enable, changedByTransport)
-        NLandUnit.SetBombardmentMode(self, enable, changedByTransport)
-        self:SetScriptBit('RULEUTC_WeaponToggle', enable)
-    end,
-
-    OnScriptBitSet = function(self, bit)
-        NLandUnit.OnScriptBitSet(self, bit)
-        if bit == 1 then
-            NLandUnit.SetBombardmentMode(self, true, false)
-        end
-    end,
-
-    OnScriptBitClear = function(self, bit)
-        NLandUnit.OnScriptBitClear(self, bit)
-        if bit == 1 then
-            NLandUnit.SetBombardmentMode(self, false, false)
-        end
-    end,
 }
 
 TypeClass = INU3003
