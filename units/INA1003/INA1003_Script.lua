d0d3c863299c8318a61087e184ff8a140554f866
Apofenas <35565415+Apofenas@users.noreply.github.com>
2018-10-27 16:40:02 +0900

    Update INA1003_Script.lua

--- units/INA1003/INA1003_Script.lua
+++ units/INA1003/INA1003_Script.lua
@@ -6,9 +6,6 @@ local RocketWeapon1Bomber = import('/lua/nomadsweapons.lua').RocketWeapon1Bomber
 INA1003 = Class(NAirUnit) {
     Weapons = {
         Rocket1 = Class(RocketWeapon1Bomber) {},
-        Rocket2 = Class(RocketWeapon1Bomber) {},
-        Rocket3 = Class(RocketWeapon1Bomber) {},
-        Rocket4 = Class(RocketWeapon1Bomber) {},
     },
 }
 
