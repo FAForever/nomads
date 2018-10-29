d51439dffaac535aeec3240a9a22aeb0e748770b
Apofenas <35565415+Apofenas@users.noreply.github.com>
2018-10-29 23:18:26 +0900

    Remove toggle button

--- units/INU1006/INU1006_Script.lua
+++ units/INU1006/INU1006_Script.lua
@@ -1,11 +1,8 @@
 -- T1 mobile AntiAir / artillery Script
 
-local AddBombardModeToUnit = import('/lua/nomadsutils.lua').AddBombardModeToUnit
 local NLandUnit = import('/lua/nomadsunits.lua').NLandUnit
 local RocketWeapon1 = import('/lua/nomadsweapons.lua').RocketWeapon1
 local TargetingLaser = import('/lua/kirvesweapons.lua').TargetingLaserInvisible
-
-NLandUnit = AddBombardModeToUnit(NLandUnit)
 
 INU1006 = Class(NLandUnit) {
     Weapons = {
@@ -40,36 +37,6 @@ INU1006 = Class(NLandUnit) {
 
     OnCreate = function(self)
         NLandUnit.OnCreate(self)
-        self:SetScriptBit('RULEUTC_SpecialToggle', false)
-    end,
-
-    SetBombardmentMode = function(self, enable, changedByTransport)
-        NLandUnit.SetBombardmentMode(self, enable, changedByTransport)
-        self:SetScriptBit('RULEUTC_WeaponToggle', enable)
-    end,
-
-    OnScriptBitSet = function(self, bit)
-        NLandUnit.OnScriptBitSet(self, bit)
-        if bit == 1 then
-            self.SetBombardmentMode(self, true, false)
-        end
-    end,
-
-    OnScriptBitClear = function(self, bit)
-        NLandUnit.OnScriptBitClear(self, bit)
-        if bit == 1 then
-            self.SetBombardmentMode(self, false, false)
-        end
-    end,
-
-    DisableSpecialToggle = function(self)
-        self:SetWeaponEnabledByLabel('TargetPainter', true)
-    end,
-
-    EnableSpecialToggle = function(self)
-        self:SetWeaponEnabledByLabel('TargetPainter', false)
-        self:SetWeaponEnabledByLabel('ArtilleryGun', true)
-        self:SetWeaponEnabledByLabel('AAGun', false)
     end,
 }
 
