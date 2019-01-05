dcf9168c018dabcd0318e8e032dd462242ec7a43
Apofenas <Apofenas@gmail.com>
2019-01-05 12:15:41 +0900

    Barrager rework into MML
    Reworks Barrager into MML
    Small tweak for AA weapon
    Improve for ground-weapon toggle

--- projectiles/NArtilleryRocket1/NArtilleryRocket1_Script.lua
+++ projectiles/NArtilleryRocket1/NArtilleryRocket1_Script.lua
@@ -1,5 +1,24 @@
 local Rocket1 = import('/lua/nomadsprojectiles.lua').Rocket1
 
-NArtilleryRocket1 = Class(Rocket1) {}
+NArtilleryRocket1 = Class(Rocket1) {
+	OnCreate = function(self)
+		Rocket1.OnCreate(self)
+        self:SetTurnRate(0)
+        self:ChangeMaxZigZag(0)
+        self:ChangeZigZagFrequency(0)
+        self:ForkThread(self.StageThread)
+    end,
+
+    StageThread = function(self)
+        WaitSeconds(2)
+        local bp = self:GetBlueprint().Physics
+        self:SetTurnRate(bp.TurnRate)
+        self:ChangeMaxZigZag(bp.MaxZigZag)
+        self:ChangeZigZagFrequency(bp.ZigZagFrequency)
+        WaitSeconds(1)
+        self:ChangeMaxZigZag(0)
+        self:ChangeZigZagFrequency(0)
+    end,
+}
 
 TypeClass = NArtilleryRocket1
