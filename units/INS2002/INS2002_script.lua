f3c73f79939caf74eb6689c5188a800330b2b8f4
Apofenas <Apofenas@gmail.com>
2019-01-05 14:13:51 +0900

    Cruiser TML & EMP tweaks
    Makes Cruiser launch manually TMLs like ACU
    EMP weapon buff

--- units/INS2002/INS2002_script.lua
+++ units/INS2002/INS2002_script.lua
@@ -4,14 +4,15 @@ local AddNavalLights = import('/lua/nomadsutils.lua').AddNavalLights
 local NSeaUnit = import('/lua/nomadsunits.lua').NSeaUnit
 local RocketWeapon1 = import('/lua/nomadsweapons.lua').RocketWeapon1
 local EMPGun = import('/lua/nomadsweapons.lua').EMPGun
+local TacticalMissileWeapon1 = import('/lua/nomadsweapons.lua').TacticalMissileWeapon2
 
 NSeaUnit = AddNavalLights(NSeaUnit)
 
 INS2002 = Class(NSeaUnit) {
     Weapons = {
         AATurret = Class(RocketWeapon1) {},
-        CruiseMissile = Class(RocketWeapon1) {},
         GunTurret = Class(EMPGun) {},
+        CruiseMissile = Class(TacticalMissileWeapon1) {},
     },
 
     LightBone_Left = 'Light2',
