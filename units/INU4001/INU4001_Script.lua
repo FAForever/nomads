99c1dd526dac52e90a7d8691dfd14212c55c0152
Apofenas <Apofenas@gmail.com>
2019-02-09 18:21:21 +0900

    Crawler bombard remove
    Removed bombardment from Crawler
    EMP missiles range and DPS increased to lvl of Torrents

--- units/INU4001/INU4001_Script.lua
+++ units/INU4001/INU4001_Script.lua
@@ -16,8 +16,6 @@ local GetRandomInt = import('/lua/utilities.lua').GetRandomInt
 local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 local SlowHover = import('/lua/defaultunits.lua').SlowHoverLandUnit
 
-NExperimentalHoverLandUnit = AddBombardModeToUnit(AddLights(NExperimentalHoverLandUnit))
-
 INU4001 = Class(NExperimentalHoverLandUnit, SlowHover) {
 
     Weapons = {
@@ -102,7 +100,6 @@ INU4001 = Class(NExperimentalHoverLandUnit, SlowHover) {
                 end
             end,
         },
-        MainGun_BombardmentMode = Class(EMPMissileWeapon) {},
         FrontGattling01 = Class(GattlingWeapon1) {},
         FrontGattling02 = Class(GattlingWeapon1) {},
         SideGunRight01 = Class(ParticleBlaster1) {},
@@ -195,27 +192,6 @@ INU4001 = Class(NExperimentalHoverLandUnit, SlowHover) {
 
     DestroyMovementSmokeEffects = function(self)
         self.SmokeEmitters:Destroy()
-    end,
-
--- BOMBARDMENT =====================
-
-    SetBombardmentMode = function(self, enable, changedByTransport)
-        NExperimentalHoverLandUnit.SetBombardmentMode(self, enable, changedByTransport)
-        self:SetScriptBit('RULEUTC_WeaponToggle', enable)
-    end,
-
-    OnScriptBitSet = function(self, bit)
-        NExperimentalHoverLandUnit.OnScriptBitSet(self, bit)
-        if bit == 1 then
-            NExperimentalHoverLandUnit.SetBombardmentMode(self, true, false)
-        end
-    end,
-
-    OnScriptBitClear = function(self, bit)
-        NExperimentalHoverLandUnit.OnScriptBitClear(self, bit)
-        if bit == 1 then
-            NExperimentalHoverLandUnit.SetBombardmentMode(self, false, false)
-        end
     end,
 
 -- DYING =====================
