f5040ebdb320d10f6ffe59e2e3dbe3812796f772
Apofenas <Apofenas@gmail.com>
2019-02-09 17:21:15 +0900

    Removal of sniper mode
    Removing sniper mode from Mauler

--- units/INU3004/INU3004_Script.lua
+++ units/INU3004/INU3004_Script.lua
@@ -4,54 +4,17 @@ local NLandUnit = import('/lua/nomadsunits.lua').NLandUnit
 local ArtilleryWeapon = import('/lua/nomadsweapons.lua').ArtilleryWeapon
 local Utilities = import('/lua/utilities.lua')
 local SupportedArtilleryWeapon = import('/lua/nomadsutils.lua').SupportedArtilleryWeapon
-
-local SeraphimWeapons = import('/lua/seraphimweapons.lua')
-local EffectUtil = import('/lua/EffectUtilities.lua')  --added for effects
-
-local DirectWeapon = SupportedArtilleryWeapon( ArtilleryWeapon )
-ArtilleryWeapon = SupportedArtilleryWeapon( ArtilleryWeapon )
-
-
---ArtilleryWeapon = SupportedArtilleryWeapon(ArtilleryWeapon)
-
-
-
+local EffectUtil = import('/lua/EffectUtilities.lua')
+local ArtilleryWeapon = SupportedArtilleryWeapon( ArtilleryWeapon )
 
 INU3004 = Class(NLandUnit) {
     Weapons = {
-        SniperGun = Class(DirectWeapon) {
-            SetOnTransport = function(self, transportstate)
-                ArtilleryGun.SetOnTransport(self, transportstate)
-                self.unit:SetScriptBit('RULEUTC_WeaponToggle', false)
-            end,
-        },
 		ArtilleryGun = Class(ArtilleryWeapon) {},
     },
 	
 	OnCreate = function(self)
         NLandUnit.OnCreate(self)
-        self:SetWeaponEnabledByLabel('ArtilleryGun', false)
     end,
-	
-	OnScriptBitSet = function(self, bit)
-        NLandUnit.OnScriptBitClear(self, bit)
-        if bit == 1 then
-			local bp = self:GetBlueprint()
-			self:SetWeaponEnabledByLabel('ArtilleryGun', true)
-			self:SetWeaponEnabledByLabel('SniperGun', false)
-            self:GetWeaponManipulatorByLabel('ArtilleryGun'):SetHeadingPitch(self:GetWeaponManipulatorByLabel('SniperGun'):GetHeadingPitch())
-		end
-	end,
-
-    OnScriptBitClear = function(self, bit)
-		NLandUnit.OnScriptBitSet(self, bit)
-        if bit == 1 then
-			local bp = self:GetBlueprint()			
-			self:SetWeaponEnabledByLabel('ArtilleryGun', false)
-			self:SetWeaponEnabledByLabel('SniperGun', true)
-            self:GetWeaponManipulatorByLabel('SniperGun'):SetHeadingPitch(self:GetWeaponManipulatorByLabel('ArtilleryGun'):GetHeadingPitch())
-		end
-	end,
 }
 
 TypeClass = INU3004
