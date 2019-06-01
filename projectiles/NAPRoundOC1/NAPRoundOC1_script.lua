0e6148e225f0ddee6dac698882de457925e559ea
Exotic-Retard <ddoogedoge@gmail.com>
2019-06-01 14:04:06 +0100

    Fix ACU OC functionally
    this makes the overcharge not broken. now it drains energy correctly and
    doesnt cause errors in the log.
    
    because the equation for OC damage is non-linear, its insane to attempt
    to split it in between projectiles in a burst, so the overcharge has
    been changed to a single projectile.
    
    there still needs to be a cosmetic distinction between regular
    projectiles and OC, which needs to be done at some time later.

---
+++ projectiles/NAPRoundOC1/NAPRoundOC1_script.lua
@@ -0,0 +1,18 @@
+﻿local APRound = import('/lua/nomadsprojectiles.lua').APRound
+local OverchargeProjectile = import('/lua/sim/DefaultProjectiles.lua').OverchargeProjectile
+
+
+NAPRoundOC1 = Class(APRound, OverchargeProjectile) {
+
+    OnImpact = function(self, targetType, targetEntity)
+        OverchargeProjectile.OnImpact(self, targetType, targetEntity)
+        APRound.OnImpact(self, targetType, targetEntity)
+    end,
+    
+    OnCreate = function(self)
+        OverchargeProjectile.OnCreate(self)
+        APRound.OnCreate(self)
+    end,
+}
+
+TypeClass = NAPRoundOC1
