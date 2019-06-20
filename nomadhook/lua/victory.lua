dec5f3cc6c096e4d34400041734d2223f4974eac
Exotic-Retard <ddoogedoge@gmail.com>
2019-06-20 22:20:23 +0100

    Change victory criteria to work with Nomads
    this is just changing the categories to include a generic exclusion
    category, and then adding it to appropriate orbital/utility units that
    shouldnt be counted in the conditions. easy and watertight.

---
+++ nomadhook/lua/victory.lua
@@ -0,0 +1,3 @@
+--we remove the category from the domination counter. done like this for maximum hookability
+victoryCategories.domination = victoryCategories.domination - categories.EXCLUDEINDOMINATION
+victoryCategories.eradication = victoryCategories.eradication - categories.EXCLUDEINDOMINATION
