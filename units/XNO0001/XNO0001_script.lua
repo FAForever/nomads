5b72e0c78e1f969c4dfd8981354081399a38f306
Exotic-Retard <ddoogedoge@gmail.com>
2019-06-23 08:38:26 +0100

    Add warp in animation to Orbital frigate
    now it has a nice unpacking animation.
    more effects are still needed though :>

--- units/XNO0001/XNO0001_script.lua
+++ units/XNO0001/XNO0001_script.lua
@@ -5,6 +5,7 @@ local OrbitalMissileWeapon = import('/lua/nomadsweapons.lua').OrbitalMissileWeap
 local CreateNomadsBuildSliceBeams = import('/lua/nomadseffectutilities.lua').CreateNomadsBuildSliceBeams
 local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 
+
 xno0001 = Class(NOrbitUnit) {
     Weapons = {
         OrbitalGun1 = Class(OrbitalMissileWeapon) {},
@@ -28,6 +29,7 @@ xno0001 = Class(NOrbitUnit) {
 
     OnCreate = function(self)
         self.BuildEffectsBag = TrashBag()
+        self.EngineEffectsBag = TrashBag()
         self.ThrusterEffectsBag = TrashBag()
 
         NOrbitUnit.OnCreate(self)
@@ -42,7 +44,7 @@ xno0001 = Class(NOrbitUnit) {
         self.OrbitalStrikeDbKey = {}
         self.OrbitalSpawnQueue = {}
         self.returnposition = Vector(self:GetPosition()[1], self:GetPosition()[2], self:GetPosition()[3]) --why is this like this
-        self.MoveAway(self)
+        self:Landing(false)
     end,
 
 -- =========================================================================================
@@ -54,12 +56,14 @@ xno0001 = Class(NOrbitUnit) {
             self.RotatorOuter = CreateRotator( self, 'Deflector Edge', 'z' )
             self.RotatorOuter:SetAccel( bp.OuterAcceleration )
             self.RotatorOuter:SetTargetSpeed( bp.OuterSpeed )
+            self.RotatorOuter:SetSpeed( bp.OuterSpeed )
             self.Trash:Add( self.RotatorOuter )
         end
         if not self.RotatorInner then
             self.RotatorInner = CreateRotator( self, 'Deflector Centre', 'z' )
             self.RotatorInner:SetAccel( bp.InnerAcceleration )
             self.RotatorInner:SetTargetSpeed( bp.InnerSpeed )
+            self.RotatorInner:SetSpeed( bp.InnerSpeed )
             self.Trash:Add( self.RotatorInner )
         end
     end,
@@ -241,58 +245,75 @@ xno0001 = Class(NOrbitUnit) {
     end,    
 
 -- engines
-    --ThrusterBurnBones = {'ExhaustBig', 'ExhaustSmallRight', 'ExhaustSmallLeft', 'ExhaustSmallTop'},
-    ThrusterBurnBones = {'Engine Exhaust01', 'Engine Exhaust02', 'Engine Exhaust03', 'Engine Exhaust04', 'Engine Exhaust05', }, --rename to EngineBones ?
-
-    BurnEngines = function(self)
-        local army, emit = self:GetArmy()
-        local ThrusterEffects = {
-            '/effects/emitters/nomads_orbital_frigate_thruster04_emit.bp',
-            '/effects/emitters/nomads_orbital_frigate_thruster05_emit.bp',
-            '/effects/emitters/nomads_orbital_frigate_thruster01_emit.bp',
-            '/effects/emitters/nomads_orbital_frigate_thruster02_emit.bp',
-        }
-        ForkThread( function()
-            for i = 1, 4 do
-                for _, bone in self.ThrusterBurnBones do
-                    emit = CreateAttachedEmitter( self, bone, army, ThrusterEffects[i] )
-                    self.ThrusterEffectsBag:Add( emit )
-                    self.Trash:Add( emit )
-                    WaitSeconds(0.2)
-                end
-                WaitSeconds(1.5)
-            end
+    EngineExhaustBones = {'Engine Exhaust01', 'Engine Exhaust02', 'Engine Exhaust03', 'Engine Exhaust04', 'Engine Exhaust05', },
+    ThrusterExhaustBones = { 'ThrusterPort01', 'ThrusterPort02', 'ThrusterPort03', 'ThrusterPort04', 'ThrusterPort05', 'ThrusterPort06', },
+    EngineFireEffects = { --for when the engine is on full power
+        '/effects/emitters/nomads_orbital_frigate_thruster04_emit.bp',--smoke
+        '/effects/emitters/nomads_orbital_frigate_thruster05_emit.bp',--smoke
+        '/effects/emitters/nomads_orbital_frigate_thruster01_emit.bp',--fire
+        '/effects/emitters/nomads_orbital_frigate_thruster02_emit.bp',--fire
+    },
+    EnginePartialEffects = { --hot air effects only
+        --'/effects/emitters/nomads_orbital_frigate_thruster03_emit.bp', --this one looks dumb
+        '/effects/emitters/nomads_orbital_frigate_thruster04_emit.bp',
+    },
+    ThrusterEffects = { --hot air effects only
+        --'/effects/emitters/nomads_orbital_frigate_thruster03_emit.bp', --this one looks dumb
+        '/effects/emitters/aeon_t1eng_groundfx01_emit.bp',
+    },
+    
+    StartEngines = function(self)
+        --TODO:Rewrite the movement mechanism so theres never a need for insane threading like this
+        self:ForkThread(function()
+            self:AddEffects(self.EngineFireEffects, self.EngineExhaustBones, self.EngineEffectsBag, 0.3)
         end)
     end,
 
     StopEngines = function(self)
-        self.ThrusterEffectsBag:Destroy()
-        local army, emit = self:GetArmy()
-        local ThrusterEffects = {
-            '/effects/emitters/nomads_orbital_frigate_thruster03_emit.bp',
-            '/effects/emitters/nomads_orbital_frigate_thruster04_emit.bp',
-        }
-        ForkThread( function()
-            for i = 1, 2 do
-                for _, bone in self.ThrusterBurnBones do
-                    emit = CreateAttachedEmitter( self, bone, army, ThrusterEffects[i] )
-                    self.ThrusterEffectsBag:Add( emit )
-                    self.Trash:Add( emit )
-                end
+        self.EngineEffectsBag:Destroy()
+        self:AddEffects(self.EnginePartialEffects, self.EngineExhaustBones, self.EngineEffectsBag)
+        WaitSeconds(4.5)
+        self.EngineEffectsBag:Destroy()
+    end,
+    
+    Landing = function (self, EnableThrusters)
+        self:HideBone(0, true)
+        --start rotators
+        self:SetupRotators()
+        self:StopRotators() --start slowing them down
+        
+        self:AddEffects(self.EngineFireEffects, self.EngineExhaustBones, self.EngineEffectsBag)
+        self.LaunchAnim = CreateAnimator(self):PlayAnim('/units/xno0001/xno0001_entry01.sca')
+        --self.LaunchAnim:SetAnimationFraction(0.3)
+        self.LaunchAnim:SetRate(0.1)
+        self.Trash:Add(self.LaunchAnim)
+        
+        self:ForkThread(function(self, EnableThrusters)
+            WaitSeconds(0.1)
+            self:ShowBone(0, true)
+            WaitSeconds(3.5)
+            if EnableThrusters then
+                self:AddEffects(self.ThrusterEffects, self.ThrusterExhaustBones, self.ThrusterEffectsBag)
             end
             WaitSeconds(1)
-            self.ThrusterEffectsBag:Destroy()
-            for _, bone in self.ThrusterBurnBones do
-                emit = CreateAttachedEmitter( self, bone, army, ThrusterEffects[2] )
-                self.ThrusterEffectsBag:Add( emit )
-                self.Trash:Add( emit )
-            end
-            WaitSeconds(5)
-            self.ThrusterEffectsBag:Destroy()
-        end)
+            self:StopEngines()
+            self.MoveAway(self)
+        end, EnableThrusters)
     end,
 
-
+    AddEffects = function (self, effects, bones, bag, delay)
+        local army, emit = self:GetArmy()
+        for _, effect in effects do
+            for _, bone in bones do
+                emit = CreateAttachedEmitter(self, bone, army, effect)
+                bag:Add(emit)
+                self.Trash:Add(emit)
+                if delay then --you need to fork the thread for that!
+                    WaitSeconds(delay)
+                end
+            end
+        end
+    end,
 
 -- movement behavior
     MoveAway = function(self)
@@ -316,7 +337,7 @@ xno0001 = Class(NOrbitUnit) {
         end
 
         self.MoveCommand = IssueMove({self}, self.targetCoordinates)
-        self:BurnEngines()
+        self:StartEngines()
 
         self:CheckIfAtTarget()
     end,
@@ -324,7 +345,7 @@ xno0001 = Class(NOrbitUnit) {
     ReturnToStartLocation = function(self)
         if self:GetPosition() == self.returnposition then return end
         self.MoveCommand = IssueMove({self}, self.returnposition)
-        self:BurnEngines()
+        self:StartEngines()
         self:CheckIfAtTarget(self.returnposition)
     end,
 
