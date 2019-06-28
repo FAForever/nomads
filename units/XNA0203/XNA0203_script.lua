79e719312441792f39701d67bf169b9b1c290343
Apofenas <Apofenas@gmail.com>
2019-06-28 19:32:41 +0900

    3704: Vangard
    Remove redundant abilities and reduce sonar radius to FAF values

--- units/XNA0203/XNA0203_script.lua
+++ units/XNA0203/XNA0203_script.lua
@@ -1,35 +1,14 @@
 -- T2 torpedo gunship
 
-local SupportingArtilleryAbility = import('/lua/nomadsutils.lua').SupportingArtilleryAbility
-local AddFlares = import('/lua/nomadsutils.lua').AddFlares
 local NAirUnit = import('/lua/nomadsunits.lua').NAirUnit
 local StingrayCannon1 = import('/lua/nomadsweapons.lua').StingrayCannon1
 local DroppedTorpedoWeapon1  = import('/lua/nomadsweapons.lua').DroppedTorpedoWeapon1
-
--- add supporting artillery ability and aa missile flares
-NAirUnit = SupportingArtilleryAbility( NAirUnit )
-NAirUnit = AddFlares( NAirUnit )
 
 XNA0203 = Class(NAirUnit) {
      Weapons = {
         MainGun = Class(StingrayCannon1) {},
         Torpedo = Class(DroppedTorpedoWeapon1) {},
     },
-
-    OnMotionVertEventChange = function( self, new, old )
-        NAirUnit.OnMotionVertEventChange( self, new, old )
-
-        -- special abilities only available when on cruising height
-        if new == 'Top' then
-            -- unit reaching target altitude, coming from surface
-            self:EnableArtillerySupport(true)
-            self:SetFlaresEnabled(true)
-        elseif new == 'Down' then
-            -- unit starts landing
-            self:EnableArtillerySupport(false)
-            self:SetFlaresEnabled(false)
-        end
-    end,
 }
 
 TypeClass = XNA0203
