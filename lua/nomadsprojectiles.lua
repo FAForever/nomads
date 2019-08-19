c8c657c47eeb49a26ad8970138260ac40f278e1b
Steven Cripe <stevenc21@protonmail.com>
2019-08-19 14:34:58 -0700

    Decrease Size of MML Effects
    Effect is obnoxious and large, which distracts from the missile itself.

--- lua/nomadsprojectiles.lua
+++ lua/nomadsprojectiles.lua
@@ -1234,7 +1234,7 @@ NIFCruiseMissile = Class(SingleCompositeEmitterProjectile) {
     -- small correction to make the smoke appear to come from the missile
     FxTrailOffset = -0.16,
     PolyTrailOffset = -0.11,
-    FxTrailScale = 0.75,
+    FxTrailScale = .5,
 
     FxImpactTrajectoryAligned = false,
 
