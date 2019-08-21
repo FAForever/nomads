7b9714c170692f0876d15170ff441201dbea1a19
Exotic-Retard <ddoogedoge@gmail.com>
2019-08-21 19:32:49 +0100

    Add Explosion templates to NomadsExplosions.lua
    This file should really be used, and so it will be. Here are some
    functions that are useful for projectiles and explosions to have, more
    will be added soon.

--- lua/nomadsexplosions.lua
+++ lua/nomadsexplosions.lua
@@ -5,6 +5,7 @@ local GetRandomFloat = util.GetRandomFloat
 local GetRandomInt = util.GetRandomInt
 local GetRandomOffset = util.GetRandomOffset
 local GetRandomOffset2 = util.GetRandomOffset2
+
 local EfctUtil = import('EffectUtilities.lua')
 local CreateEffects = EfctUtil.CreateEffects
 local CreateEffectsWithOffset = EfctUtil.CreateEffectsWithOffset
@@ -14,6 +15,47 @@ local CreateBoneEffectsOffset = EfctUtil.CreateBoneEffectsOffset
 local CreateRandomEffects = EfctUtil.CreateRandomEffects
 local ScaleEmittersParam = EfctUtil.ScaleEmittersParam
 
+
+local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
+
+--TODO:Remove this?
 function CreateFlashCustom( obj, bone, army, scale, duration, textureflash, rampflash )
     CreateLightParticle( obj, bone, army, scale, duration, textureflash, rampflash )
+end
+
+function CreateExplosionMedium(self, pos, army, targetType)
+    if targetType == 'Terrain' then
+        DamageArea(self, pos, self.DamageData.DamageRadius * 1.2, 1, 'Force', true)
+    end
+
+    CreateLightParticle( self, -2, army, 2.5, 5*2, 'glow_05_red', 'ramp_jammer_01' )
+    CreateLightParticle( self, -2, army, 3, 2*2, 'glow_05_red', 'ramp_jammer_01' )
+
+    -- create some additional effects
+    if targetType ~= 'Water' and targetType ~= 'Shield' and targetType ~= 'Air' and targetType ~= 'UnitAir' and targetType ~= 'UnitUnderwater' then
+        -- CreateScorchMarkDecal(pos, army, 'Scorch_012_albedo', 4, 5.5, 100, 150) --not a smart idea to leave decals on something that fires so fast
+        if self.DoImpactFlash then
+            CreateLightParticle( self, -1, army, 6, 5, 'glow_03', 'ramp_yellow_blue_01' )
+            CreateLightParticle( self, -1, army, 8, 16, 'glow_03', 'ramp_antimatter_02' )
+        end
+    end
+end
+
+function CreateArtilleryExplosionLarge(self, pos, army, targetType)
+    DamageArea(self, pos, self.DamageData.DamageRadius * 3, 1, 'Force', true)
+
+    if self.DoImpactFlash then
+        CreateLightParticle( self, -1, army, 16, 6, 'glow_03', 'ramp_antimatter_02' )
+    end
+
+    if targetType ~= 'Water' and targetType ~= 'Shield' and targetType ~= 'Air' and targetType ~= 'UnitAir' then
+        CreateScorchMarkDecal(pos, army, 'nuke_scorch_002_albedo', 13, 16, 40, 60)
+    end
+end
+
+function CreateScorchMarkDecal(pos, army, blueprint, SizeMin, SizeMax, LifeMin, LifeMax)
+    local rotation = RandomFloat(0,2*math.pi)
+    local size = RandomFloat(SizeMin or 13, SizeMax or 16)
+    local life = Random(LifeMin or 40, LifeMax or 60)
+    CreateDecal(pos, rotation, blueprint, '', 'Albedo', size, size, 300, life, army)
 end
