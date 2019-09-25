d330d7628eb49e0761cfdadcad692d3cdc6659af
Exotic-Retard <ddoogedoge@gmail.com>
2019-09-25 16:57:46 +0100

    Refactor Command frigate code to be inherited
    now the campaign and ingame versions of the frigates both use the same
    animations and effects code, which means reduced file size and easier
    maintenance!

--- units/XNO0001/XNO0001_script.lua
+++ units/XNO0001/XNO0001_script.lua
@@ -1,10 +1,11 @@
 -- The surface support vehicle that's in orbit
 
 local NOrbitUnit = import('/lua/nomadsunits.lua').NOrbitUnit
+local NCommandFrigateUnit = import('/lua/nomadsunits.lua').NCommandFrigateUnit
 local NIFOrbitalBombardmentWeapon = import('/lua/nomadsweapons.lua').NIFOrbitalBombardmentWeapon
 
 
-xno0001 = Class(NOrbitUnit) {
+xno0001 = Class(NOrbitUnit, NCommandFrigateUnit) {
     Weapons = {
         OrbitalMissileLauncher = Class(NIFOrbitalBombardmentWeapon) {},
         OrbitalMissileLauncherHeavy = Class(NIFOrbitalBombardmentWeapon) {},
@@ -14,15 +15,13 @@ xno0001 = Class(NOrbitUnit) {
     BuildBones = { 0, },
     BuildEffectsBag = nil,
     ThrusterEffectsBag = nil,
-
+    
     OnCreate = function(self)
-        self.BuildEffectsBag = TrashBag()
         self.EngineEffectsBag = TrashBag()
         self.ThrusterEffectsBag = TrashBag()
-
+        self.BuildEffectsBag = TrashBag()
         NOrbitUnit.OnCreate(self)
-
-        --self:SetupRotators()
+        
         local bp = self:GetBlueprint()
         if bp.Display.AnimationBuildArm then
             self.ConstructionArmAnimManip = CreateAnimator( self ):PlayAnim( bp.Display.AnimationBuildArm ):SetRate(0)
@@ -35,46 +34,10 @@ xno0001 = Class(NOrbitUnit) {
         self:Landing(false)
     end,
 
--- =========================================================================================
--- Rotators
-
-    SetupRotators = function(self)
-        local bp = self:GetBlueprint().Rotators
-        if not self.RotatorOuter then
-            self.RotatorOuter = CreateRotator( self, 'Deflector Edge', 'z' )
-            self.RotatorOuter:SetAccel( bp.OuterAcceleration )
-            self.RotatorOuter:SetTargetSpeed( bp.OuterSpeed )
-            self.RotatorOuter:SetSpeed( bp.OuterSpeed )
-            self.Trash:Add( self.RotatorOuter )
-        end
-        if not self.RotatorInner then
-            self.RotatorInner = CreateRotator( self, 'Deflector Centre', 'z' )
-            self.RotatorInner:SetAccel( bp.InnerAcceleration )
-            self.RotatorInner:SetTargetSpeed( bp.InnerSpeed )
-            self.RotatorInner:SetSpeed( bp.InnerSpeed )
-            self.Trash:Add( self.RotatorInner )
-        end
+    LandingThread = function(self, EnableThrusters)
+        NCommandFrigateUnit.LandingThread(self, EnableThrusters)
+        self.MovementThread(self) --after landing enable following its target
     end,
-
-    StopRotators = function(self)
-        if self.RotatorOuter then
-            self.RotatorOuter:SetTargetSpeed( 0 )
-        end
-        if self.RotatorInner then
-            self.RotatorInner:SetTargetSpeed( 0 )
-        end      
-    end,
-    
-    StartRotators = function(self)
-        local bp = self:GetBlueprint().Rotators
-        if self.RotatorOuter then
-            self.RotatorOuter:SetTargetSpeed( bp.OuterSpeed )
-        end
-        if self.RotatorInner then
-            self.RotatorInner:SetTargetSpeed( bp.InnerSpeed )
-        end
-    end,
-
 -- =========================================================================================
 -- Probes
 
@@ -126,11 +89,6 @@ xno0001 = Class(NOrbitUnit) {
         local weapon = self:GetWeaponByLabel(weaponLabel)
         weapon:SetTargetGround( targetPosition )
         weapon:OnFire()
-    end,
-
-    --this worked perfectly so long as there was only one weapon to fire, now its obsolete
-    OnGivenNewTargetOld = function(self, targetPosition)
-        IssueTactical( {self}, targetPosition )
     end,
 
 -- =========================================================================================
@@ -248,74 +206,10 @@ xno0001 = Class(NOrbitUnit) {
         return spin, fx, fy, fz
     end,    
 
--- =========================================================================================
--- engines
-    EngineExhaustBones = {'Engine Exhaust01', 'Engine Exhaust02', 'Engine Exhaust03', 'Engine Exhaust04', 'Engine Exhaust05', },
-    ThrusterExhaustBones = { 'ThrusterPort01', 'ThrusterPort02', 'ThrusterPort03', 'ThrusterPort04', 'ThrusterPort05', 'ThrusterPort06', },
-    EngineFireEffects = { --for when the engine is on full power
-        '/effects/emitters/nomads_orbital_frigate_thruster04_emit.bp',--smoke
-        '/effects/emitters/nomads_orbital_frigate_thruster05_emit.bp',--smoke
-        '/effects/emitters/nomads_orbital_frigate_thruster01_emit.bp',--fire
-        '/effects/emitters/nomads_orbital_frigate_thruster02_emit.bp',--fire
-    },
-    EnginePartialEffects = { --hot air effects only
-        --'/effects/emitters/nomads_orbital_frigate_thruster03_emit.bp', --this one looks dumb
-        '/effects/emitters/nomads_orbital_frigate_thruster04_emit.bp',
-    },
-    ThrusterEffects = { --hot air effects only
-        --'/effects/emitters/nomads_orbital_frigate_thruster03_emit.bp', --this one looks dumb
-        '/effects/emitters/aeon_t1eng_groundfx01_emit.bp',
-    },
 
-    StopEngines = function(self)
-        self.EngineEffectsBag:Destroy()
-        self:AddEffects(self.EnginePartialEffects, self.EngineExhaustBones, self.EngineEffectsBag)
-        WaitSeconds(4.5)
-        self.EngineEffectsBag:Destroy()
-    end,
-    
-    Landing = function (self, EnableThrusters)
-        self:HideBone(0, true)
-        --start rotators
-        self:SetupRotators()
-        self:StopRotators() --start slowing them down
-        
-        self:AddEffects(self.EngineFireEffects, self.EngineExhaustBones, self.EngineEffectsBag)
-        self.LaunchAnim = CreateAnimator(self):PlayAnim('/units/xno0001/XNO0001_WarpEntry01.sca')
-        --self.LaunchAnim:SetAnimationFraction(0.3)
-        self.LaunchAnim:SetRate(0.1)
-        self.Trash:Add(self.LaunchAnim)
-        
-        self:ForkThread(function(self, EnableThrusters)
-            WaitSeconds(0.1)
-            self:ShowBone(0, true)
-            WaitSeconds(3.5)
-            if EnableThrusters then
-                self:AddEffects(self.ThrusterEffects, self.ThrusterExhaustBones, self.ThrusterEffectsBag)
-            end
-            WaitSeconds(1)
-            self:StopEngines()
-            --self.MoveAway(self)
-            self.MovementThread(self)
-        end, EnableThrusters)
-    end,
-
-    AddEffects = function (self, effects, bones, bag, delay)
-        local army, emit = self:GetArmy()
-        for _, effect in effects do
-            for _, bone in bones do
-                emit = CreateAttachedEmitter(self, bone, army, effect)
-                bag:Add(emit)
-                self.Trash:Add(emit)
-                if delay then --you need to fork the thread for that!
-                    WaitSeconds(delay)
-                end
-            end
-        end
-    end,
 
 -- =========================================================================================
--- movement behavior
+-- movement behaviour
 
     MovementThread = function(self)
         while self and not self:BeenDestroyed() do
