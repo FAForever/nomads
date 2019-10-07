3cf6d1606897b8f11ec1bb3d284526d007487824
Exotic-Retard <ddoogedoge@gmail.com>
2019-10-07 11:30:17 +0100

    Rework T1 MAA/A Targeting behaviour
    previously the maaa would target air too much or when toggled be a pain
    in the ass due to being unable to target ground units, which made the
    unit pretty frustrating to use.
    
    Now its behaviour is much smarter, allowing manual target overrides and
    similar. Overall this should need much less toggling and be much nicer
    to use.

--- units/XNL0103/XNL0103_Script.lua
+++ units/XNL0103/XNL0103_Script.lua
@@ -7,47 +7,84 @@ local TargetingLaser = import('/lua/kirvesweapons.lua').TargetingLaserInvisible
 XNL0103 = Class(NLandUnit) {
     Weapons = {
         TargetPainter = Class(TargetingLaser) {
-            -- Unit in range. Cease ground fire and turn on AA
             OnWeaponFired = function(self)
-                if self.AA == false then
-                    self.unit:SetWeaponEnabledByLabel('ArtilleryGun', false)
-                    self.unit:SetWeaponEnabledByLabel('AAGun', true)
-                    self.unit:GetWeaponManipulatorByLabel('AAGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('ArtilleryGun'):GetHeadingPitch())
-                    self.AA = true
-                end
+                self.unit:SetWeaponAAMode(self.unit:DetermineTargetLayer(self))
                 TargetingLaser.OnWeaponFired(self)
             end,
-
-            IdleState = State(TargetingLaser.IdleState) {
-                -- Start with the AA gun off to reduce twitching of ground fire
-                Main = function(self)
-                    if self.AA == true or self.AA == nil then
-                        self.unit:SetWeaponEnabledByLabel('ArtilleryGun', true)
-                        self.unit:SetWeaponEnabledByLabel('AAGun', false)
-                        self.unit:GetWeaponManipulatorByLabel('ArtilleryGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('AAGun'):GetHeadingPitch())
-                        self.AA = false
-                    end
-                    TargetingLaser.IdleState.Main(self)
-                end,
-            },
         },
         AAGun = Class(RocketWeapon1) {},
         ArtilleryGun = Class(RocketWeapon1) {},
     },
-
+    
     OnCreate = function(self)
         NLandUnit.OnCreate(self)
-        self:SetScriptBit('RULEUTC_SpecialToggle', false)
+        self:SetScriptBit('RULEUTC_SpecialToggle', true)
+        self:SetWeaponAAMode(false)
+        --the AA and artillery weapons share the same unpack animation, so when switching between them things dont go wrong
+        if not self.AnimationManipulator then
+            self.AnimationManipulator = CreateAnimator(self):PlayAnim('/units/XNL0103/XNL0103_Unpack.sca'):SetRate(0)
+            self.Trash:Add(self.AnimationManipulator)
+        end
     end,
-
+    
+    OnGotTarget = function(self, Weapon)
+        if Weapon:GetBlueprint().Label ~= 'TargetPainter' and self.AnimationManipulator then
+                self.AnimationManipulator:SetRate(1.5)
+        end
+        NLandUnit.OnGotTarget(self, Weapon)
+    end,
+    
+    OnLostTarget = function(self, Weapon)
+        if self.AnimationManipulator and not (self:GetWeapon(2):WeaponHasTarget() or self:GetWeapon(3):WeaponHasTarget()) then
+            self.AnimationManipulator:SetRate(-1.5)
+        end
+        NLandUnit.OnLostTarget(self, Weapon)
+    end,
+    
+    --TODO:make these more refined
+    ArtilleryModePriorities = {
+        'SPECIALHIGHPRI',
+        'STRUCTURE DEFENSE',
+        'LAND',
+        'SPECIALLOWPRI',
+        'ALLUNITS',
+    },
+    
+    --the toggles switch target priorities on the painter, which in turn disables and enables AA/artillery as needed
     DisableSpecialToggle = function(self)
-        self:SetWeaponEnabledByLabel('TargetPainter', true)
+        local weapon = self:GetWeaponByLabel('TargetPainter')
+        weapon:SetWeaponPriorities(self.ArtilleryModePriorities)
+        weapon:ResetTarget()
     end,
 
     EnableSpecialToggle = function(self)
-        self:SetWeaponEnabledByLabel('TargetPainter', false)
-        self:SetWeaponEnabledByLabel('ArtilleryGun', true)
-        self:SetWeaponEnabledByLabel('AAGun', false)
+        local weapon = self:GetWeaponByLabel('TargetPainter')
+        local bp = weapon:GetBlueprint()
+        weapon:SetWeaponPriorities(bp.TargetPriorities)
+        weapon:ResetTarget()
+    end,
+    
+    DetermineTargetLayer = function(unit, weapon)
+        local target = weapon:GetCurrentTarget()
+        local AAMode = false --default to false so that we can always attack land targets, such as groundfire which isnt a target
+        if target then
+            if not IsUnit(target) then target = target:GetSource() end --sometimes we target blips instead, which breaks getting their layer
+            AAMode = target:GetCurrentLayer() == 'Air'
+        end
+        return AAMode
+    end,
+    
+    SetWeaponAAMode = function(self, ModeEnable)
+        local weaponEnable,weaponDisable = 'ArtilleryGun', 'AAGun'
+        if self.AA == ModeEnable then return end --only toggle if we arent already toggled
+        if ModeEnable then --swap the weapons
+            weaponEnable,weaponDisable = weaponDisable,weaponEnable
+        end
+        
+        self:SetWeaponEnabledByLabel(weaponEnable, true)
+        self:SetWeaponEnabledByLabel(weaponDisable, false)
+        self:GetWeaponManipulatorByLabel(weaponEnable):SetHeadingPitch(self:GetWeaponManipulatorByLabel(weaponDisable):GetHeadingPitch())
+        self.AA = ModeEnable
     end,
 }
 
