a3e9fc811b50e1e08f8712ee1a9263954a4f9e9f
Exotic-Retard <ddoogedoge@gmail.com>
2019-10-22 17:10:17 +0100

    Create a basic working toggle
    This replaces the radar boost ability for the radar with a toggle
    button. Right now this is to test the functionality. It works but we
    should still add things like effects, and do code cleanup.

--- units/XNB3101/XNB3101_Script.lua
+++ units/XNB3101/XNB3101_Script.lua
@@ -4,7 +4,7 @@ local NRadarUnit = import('/lua/nomadsunits.lua').NRadarUnit
 local AddIntelOvercharge = import('/lua/nomadsutils.lua').AddIntelOvercharge
 local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 
-NRadarUnit = AddIntelOvercharge( NRadarUnit )
+--NRadarUnit = AddIntelOvercharge( NRadarUnit )
 
 XNB3101 = Class(NRadarUnit) {
 
@@ -16,56 +16,28 @@ XNB3101 = Class(NRadarUnit) {
     OverchargeRecoveryFx = NomadsEffectTemplate.T1RadarOverchargeRecovery,
     OverchargeChargingFx = NomadsEffectTemplate.T1RadarOverchargeCharging,
     OverchargeExplosionFx = NomadsEffectTemplate.T1RadarOverchargeExplosion,
-
+    
+    OnCreate = function(self)
+        NRadarUnit.OnCreate(self)
+        local bp = self:GetBlueprint()
+        self.IntelBoostMult = bp.Intel.IntelBoostMult
+        self.DefaultIntelRadius = bp.Intel.RadarRadius
+    end,
+    
     OnScriptBitSet = function(self, bit)
         NRadarUnit.OnScriptBitSet(self, bit)
         if bit == 1 then
-            self:IntelOverchargeBeginCharging()
+            self:SetIntelRadius('Radar', (self.DefaultIntelRadius * self.IntelBoostMult) or 170)
+            self:SetConsumptionPerSecondEnergy(50)
         end
     end,
 
     OnScriptBitClear = function(self, bit)
         NRadarUnit.OnScriptBitClear(self, bit)
         if bit == 1 then
-            self:IntelOverchargeChargingCancelled()
+            self:SetIntelRadius('Radar', self.DefaultIntelRadius or 115)
+            self:SetConsumptionPerSecondEnergy(20)
         end
-    end,
-
-    OnIntelOverchargeBeginCharging = function(self)
-        NRadarUnit.OnIntelOverchargeBeginCharging(self)
-        self:SetScriptBit('RULEUTC_WeaponToggle', true)
-    end,
-
-    OnIntelOverchargeChargingCancelled = function(self)
-        NRadarUnit.OnIntelOverchargeChargingCancelled(self)
-        self:SetScriptBit('RULEUTC_WeaponToggle', false)
-    end,
-
-    OnIntelOverchargeFinishedCharging = function(self)
-        NRadarUnit.OnIntelOverchargeFinishedCharging(self)
-        self:RemoveToggleCap('RULEUTC_WeaponToggle')
-    end,
-
-    OnBeginIntelOvercharge = function(self)
-        NRadarUnit.OnBeginIntelOvercharge(self)
-        self:RemoveToggleCap('RULEUTC_WeaponToggle')
-    end,
-
-    OnFinishedIntelOvercharge = function(self)
-        NRadarUnit.OnFinishedIntelOvercharge(self)
-
-        local OverchargeRecoverTime = self:GetBlueprint().Intel.OverchargeRecoverTime or 0
-        if OverchargeRecoverTime <= 0 then
-            self:AddToggleCap('RULEUTC_WeaponToggle')
-            self:SetScriptBit('RULEUTC_WeaponToggle', false)
-        end
-    end,
-
-    OnFinishedIntelOverchargeRecovery = function(self)
-        NRadarUnit.OnFinishedIntelOverchargeRecovery(self)
-
-        self:AddToggleCap('RULEUTC_WeaponToggle')
-        self:SetScriptBit('RULEUTC_WeaponToggle', false)
     end,
 }
 
