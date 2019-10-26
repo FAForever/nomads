7bb275f336d5493d76bf69f60e6320d6cf4be041
Exotic-Retard <ddoogedoge@gmail.com>
2019-10-26 12:45:03 +0100

    Enable Toggle functionality on Sonars
    Pretty much what it says on the tin, now the sonars work nice and neatly
    with a simple toggle.

--- units/XNB3102/XNB3102_script.lua
+++ units/XNB3102/XNB3102_script.lua
@@ -1,72 +1,18 @@
 -- T1 sonar
 
-local NRadarUnit = import('/lua/nomadsunits.lua').NRadarUnit
-local AddIntelOvercharge = import('/lua/nomadsutils.lua').AddIntelOvercharge
+local NSonarUnit = import('/lua/nomadsunits.lua').NSonarUnit
 local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 
-NRadarUnit = AddIntelOvercharge( NRadarUnit )
+XNB3102 = Class(NSonarUnit) {
 
-XNB3102 = Class(NRadarUnit) {
-
-    OverchargeFxBone = 0,
+    IntelBoostFxBone = 0,
     OverchargeChargingFxBone = 0,
     OverchargeExplosionFxBone = 0,
 
-    OverchargeFx = NomadsEffectTemplate.T1SonarOvercharge,
+    IntelBoostFx = NomadsEffectTemplate.T1SonarOvercharge,
     OverchargeRecoveryFx = NomadsEffectTemplate.T1SonarOverchargeRecovery,
     OverchargeChargingFx = NomadsEffectTemplate.T1SonarOverchargeCharging,
     OverchargeExplosionFx = NomadsEffectTemplate.T1SonarOverchargeExplosion,
-
-    OnScriptBitSet = function(self, bit)
-        NRadarUnit.OnScriptBitSet(self, bit)
-        if bit == 1 then
-            self:IntelOverchargeBeginCharging()
-        end
-    end,
-
-    OnScriptBitClear = function(self, bit)
-        NRadarUnit.OnScriptBitClear(self, bit)
-        if bit == 1 then
-            self:IntelOverchargeChargingCancelled()
-        end
-    end,
-
-    OnIntelOverchargeBeginCharging = function(self)
-        NRadarUnit.OnIntelOverchargeBeginCharging(self)
-        self:SetScriptBit('RULEUTC_WeaponToggle', true)
-    end,
-
-    OnIntelOverchargeChargingCancelled = function(self)
-        NRadarUnit.OnIntelOverchargeChargingCancelled(self)
-        self:SetScriptBit('RULEUTC_WeaponToggle', false)
-    end,
-
-    OnIntelOverchargeFinishedCharging = function(self)
-        NRadarUnit.OnIntelOverchargeFinishedCharging(self)
-        self:RemoveToggleCap('RULEUTC_WeaponToggle')
-    end,
-
-    OnBeginIntelOvercharge = function(self)
-        NRadarUnit.OnBeginIntelOvercharge(self)
-        self:RemoveToggleCap('RULEUTC_WeaponToggle')
-    end,
-
-    OnFinishedIntelOvercharge = function(self)
-        NRadarUnit.OnFinishedIntelOvercharge(self)
-
-        local OverchargeRecoverTime = self:GetBlueprint().Intel.OverchargeRecoverTime or 0
-        if OverchargeRecoverTime <= 0 then
-            self:AddToggleCap('RULEUTC_WeaponToggle')
-            self:SetScriptBit('RULEUTC_WeaponToggle', false)
-        end
-    end,
-
-    OnFinishedIntelOverchargeRecovery = function(self)
-        NRadarUnit.OnFinishedIntelOverchargeRecovery(self)
-
-        self:AddToggleCap('RULEUTC_WeaponToggle')
-        self:SetScriptBit('RULEUTC_WeaponToggle', false)
-    end,
 }
 
 TypeClass = XNB3102
