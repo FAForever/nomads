8254d4ab903bde04321018319f9762504b4c4f4d
Exotic-Retard <ddoogedoge@gmail.com>
2019-11-03 11:07:52 +0000

    Rewrite FactionConvert in ScenarioUtilities.lua
    This was ugly and hard coded, now its beautiful, hookable, dynamic and
    robust. Also faster, and less code.
    
    Right now the nomads have no template overrides, but probably need a
    couple. Still it works in 99% of cases so thats much improved already.

--- nomadhook/lua/sim/ScenarioUtilities.lua
+++ nomadhook/lua/sim/ScenarioUtilities.lua
@@ -7,50 +7,45 @@
 ----[                                                                             ]--
 ----[  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.             ]--
 local Entity = import('/lua/sim/Entity.lua').Entity
+local Factions = import('/lua/factions.lua').GetFactions(true)
 
+-- We replace the original function with this one that supports any faction number, and is also faster, and more robust.
 
--- TODO: This really ought to be hooked.... this file needs to be made game agnostic as it's in mohodata
+local FactionConversionTable = {
+    aeon = {
+        uel0203 = 'xal0203',
+        uea0305 = 'xaa0305',
+    },
+    cybran = {
+        uea0305 = 'xra0305',
+        uel0307 = 'url0306',
+    },
+    seraphim = {
+        uel0106 = 'xsl0201',
+    },
+    nomads = {
+    },
+}
 
---Rather Scary IfElse trains here, would be great to make it compatible with whatever number of factions.
 function FactionConvert(template, factionIndex)
-    local i = 3
-    while i <= table.getn(template) do
-        if factionIndex == 2 then
-            if template[i][1] == 'uel0203' then
-                template[i][1] = 'xal0203'
-            elseif template[i][1] == 'xes0204' then
-                template[i][1] = 'xas0204'
-            elseif template[i][1] == 'uea0305' then
-                template[i][1] = 'xaa0305'
-            elseif template[i][1] == 'xel0305' then
-                template[i][1] = 'xal0305'
+    local factionKey = Factions[factionIndex].Key
+    for i = 3,table.getn(template) do --start from the third item in the list onwards, for whatever reason
+        if template[i][1] == FactionConversionTable[factionKey][template[i][1]] then
+            template[i][1] = FactionConversionTable[factionKey][template[i][1]]
+        else
+            --grab first two characters from the faction bp code, we use BuildingIdPrefixes for that.
+            local factionKeyString = string.sub((Factions[factionIndex].GAZ_UI_Info.BuildingIdPrefixes[1] or "zzz"),1,2)
+            
+            --if its a vanilla faction they also contain X and D and other codes in the blueprints so we can convert them too
+            if string.sub(factionKeyString,1,1) == "u" then
+                --only swap second character
+                factionKeyString = "%1"..string.sub(factionKeyString,2,2).."%3"
             else
-                template[i][1] = string.gsub(template[i][1], 'ue', 'ua')
+                --swap first and second characters
+                factionKeyString = factionKeyString.."%3"
             end
-        elseif factionIndex == 3 then
-            if template[i][1] == 'uea0305' then
-                template[i][1] = 'xra0305'
-            elseif template[i][1] == 'xes0204' then
-                template[i][1] = 'xrs0204'
-            elseif template[i][1] == 'xes0205' then
-                template[i][1] = 'xrs0205'
-            elseif template[i][1] == 'xel0305' then
-                template[i][1] = 'xrl0305'
-            elseif template[i][1] == 'uel0307' then
-                template[i][1] = 'url0306'
-            elseif template[i][1] == 'del0204' then
-                template[i][1] = 'drl0204'
-            else
-                template[i][1] = string.gsub(template[i][1], 'ue', 'ur')
-            end
-        elseif factionIndex == 4 then
-            if template[i][1] == 'uel0106' then
-                template[i][1] = 'xsl0201'
-            elseif template[i][1] == 'xel0305' then
-                template[i][1] = 'xsl0305'
-            else
-                template[i][1] = string.gsub(template[i][1], 'ue', 'xs')
-            end
+            
+            template[i][1] = string.gsub(template[i][1], "(%w)(%w)(%w%d%d%d%d)", factionKeyString)
         end
         i = i + 1
     end
