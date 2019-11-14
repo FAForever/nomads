81b025bef7224413c689153d017a93d138aac886
Exotic-Retard <ddoogedoge@gmail.com>
2019-11-14 09:12:58 +0000

    Fix base game bug with transports inside carriers
    yeah so normally this is way outside of the scope of nomads, but in this
    case we kinda have to fix this bug since its pretty game breaking, and
    the T4 transport being able to load carriers would bring this issue to
    public attention.

--- nomadhook/lua/defaultunits.lua
+++ nomadhook/lua/defaultunits.lua
@@ -15,3 +15,27 @@ AirUnit = Class(oldAirUnit) {
     end,
 
 }
+
+-- Fix problems with aircraft carriers handling transports inside transports.
+local oldAircraftCarrier = AircraftCarrier
+AircraftCarrier = Class(oldAircraftCarrier) {
+    OnKilled = function(self, instigator, type, overkillRatio)
+        --self:SaveCargoMass() --no need to run this twice
+        self:DetachCarrierCargo()
+        oldAircraftCarrier.OnKilled(self, instigator, type, overkillRatio)
+    end,
+
+    -- This is a copy of DetachCargo, saved so that no crazy patches to the base game disrupt this functionality.
+    DetachCarrierCargo = function(self)
+        local cargo = self:GetCargo()
+        for _, unit in cargo do
+            if EntityCategoryContains(categories.TRANSPORTATION, unit) then -- Kill the contents of a transport in a transport, however that happened
+                for k, subUnit in unit:GetCargo() do
+                    subUnit:Kill()
+                end
+            end
+            unit:DetachFrom()
+        end
+    end,
+
+}
