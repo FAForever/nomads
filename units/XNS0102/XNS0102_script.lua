2128572dbd6a558f2d72d65e246158aa2ea87ffc
Exotic-Retard <ddoogedoge@gmail.com>
2019-11-14 13:09:13 +0000

    Refactor TMD on T2 Railgun carrier
    Pretty much what it says on the tin, we now make use of the new shiny
    TMD code.

--- units/XNS0102/XNS0102_script.lua
+++ units/XNS0102/XNS0102_script.lua
@@ -4,7 +4,7 @@ local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 local AddNavalLights = import('/lua/nomadsutils.lua').AddNavalLights
 local NSeaUnit = import('/lua/nomadsunits.lua').NSeaUnit
 local UnderwaterRailgunWeapon1 = import('/lua/nomadsweapons.lua').UnderwaterRailgunWeapon1
-local HVFlakWeapon = import('/lua/nomadsweapons.lua').HVFlakWeapon
+local NAMFlakWeapon = import('/lua/nomadsweapons.lua').NAMFlakWeapon
 local Utilities = import('/lua/utilities.lua')
 
 NSeaUnit = AddNavalLights(NSeaUnit)
@@ -13,68 +13,9 @@ xns0102 = Class(NSeaUnit) {
     Weapons = {
         MainGun = Class(UnderwaterRailgunWeapon1) {},
         RearGun = Class(UnderwaterRailgunWeapon1) {},
-        TMD = Class(HVFlakWeapon) { --TODO: refactor this so all the counting is done in the HVFlakWeapon and here you just set parameters
-            IdleState = State(HVFlakWeapon.IdleState) {
-                Main = function(self)
-                    HVFlakWeapon.IdleState.Main(self)
-                    self.unit:OnTargetLost()
-                    if self.IdleReloadThread then
-                        KillThread(self.IdleReloadThread)
-                    end
-                    self.IdleReloadThread = self:ForkThread(self.ReloadThread)
-                end,
-
-
-                ReloadThread = function(self)
-                    WaitSeconds(1.2)
-                    if not self.PlayingTAEffects then
-                        self.counter = 0
-                    end
-                end,
-            },
-
-            RackSalvoReloadState = State(HVFlakWeapon.RackSalvoReloadState) {
-                Main = function(self)
-                    ForkThread(function()
-                        WaitSeconds(1.2)
-                        if not self or self:BeenDestroyed() then return end
-                        HVFlakWeapon.RackSalvoReloadState.Main(self)
-                        self.unit:OnTargetLost()
-                    end)
-                end,
-            },
-
-            RackSalvoFireReadyState = State(HVFlakWeapon.RackSalvoFireReadyState ) {
-                Main = function(self)
-                    HVFlakWeapon.RackSalvoFireReadyState.Main(self)
-                    self.unit:OnTargetAcquired()
-                end,
-            },
-
-            RackSalvoFiringState = State(HVFlakWeapon.RackSalvoFiringState ) {
-                Main = function(self)
-                    if not self.counter then
-                        self.counter = 0
-                    end
-                    self.counter = self.counter + 1
-                    if self.counter > 5 then
-                        self.counter = 0
-                        self.RackSalvoReloadState.Main(self)
-                    else
-                        self:PlaySound(self.Audio.FireSpecial)
-                        HVFlakWeapon.RackSalvoFiringState.Main(self)
-                        self.unit:OnTargetAcquired()
-                    end
-                end,
-
-                Audio = {
-                    FireSpecial = Sound {
-                        Bank = 'NomadsWeapons',
-                        Cue = 'DarkMatterCannon2_Muzzle',
-                        LodCutoff = 'Weapon_LodCutoff',
-                    },
-                },
-            },
+        TMD = Class(NAMFlakWeapon) {
+            SalvoReloadTime = 1.4, --Change this to the correct amount for the weapon.
+            TMDEffectBones = { 'TMD_Fx1', 'TMD_Fx2', },
         },
     },
 
@@ -82,12 +23,11 @@ xns0102 = Class(NSeaUnit) {
     LightBone_Left = 'Antennae_2',
     LightBone_Right = 'Antennae_1',
     SmokeEmitterBones = { 'Reactor_Smoke', },
-    TMDEffectBones = { 'TMD_Fx1', 'TMD_Fx2', },
+    
 
     OnCreate = function(self)
         NSeaUnit.OnCreate(self)
         self.SmokeEmitters = TrashBag()
-        self.TAEffectsBag = TrashBag()
     end,
 
     DestroyAllDamageEffects = function(self)
@@ -102,16 +42,7 @@ xns0102 = Class(NSeaUnit) {
 
     OnDestroy = function(self)
         self:DestroyMovementSmokeEffects()
-        self:DestroyTAEffects()
         NSeaUnit.OnDestroy(self)
-    end,
-
-    OnTargetAcquired = function(self)
-        self:PlayTAEffects()
-    end,
-
-    OnTargetLost = function(self)
-        self:DestroyTAEffects()
     end,
 
     OnMotionHorzEventChange = function( self, new, old )
@@ -148,30 +79,6 @@ xns0102 = Class(NSeaUnit) {
 
     DestroyMovementSmokeEffects = function(self)
         self.SmokeEmitters:Destroy()
-    end,
-
-    PlayTAEffects = function(self)
-        if not self.PlayingTAEffects then
-            local army, emit = self:GetArmy()
-            for _, bone in self.TMDEffectBones do
-                for k, v in NomadsEffectTemplate.T2MobileTacticalMissileDefenseTargetAcquired do
-                    emit = CreateAttachedEmitter(self, bone, army, v)
-                    self.TAEffectsBag:Add(emit)
-                    self.Trash:Add(emit)
-                end
-            end
-            local thread = function(self)
-                WaitSeconds(1)
-                self:DestroyTAEffects()
-            end
-            self.PlayingTAEffectsThread = self:ForkThread( thread )
-            self.PlayingTAEffects = true
-        end
-    end,
-
-    DestroyTAEffects = function(self)
-        self.TAEffectsBag:Destroy()
-        self.PlayingTAEffects = false
     end,
 }
 
