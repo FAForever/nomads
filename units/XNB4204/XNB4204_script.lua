f56b6f70fb8c934f31a842df322e85efbf265c8f
Exotic-Retard <ddoogedoge@gmail.com>
2019-11-14 13:11:07 +0000

    Refactor TMD on T2 static TMD
    Pretty much what it says on the tin, we now make use of the new shiny
    TMD code.

--- units/XNB4204/XNB4204_script.lua
+++ units/XNB4204/XNB4204_script.lua
@@ -1,123 +1,22 @@
 -- TMD
 
-local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 local AddLights = import('/lua/nomadsutils.lua').AddLights
 local NStructureUnit = import('/lua/nomadsunits.lua').NStructureUnit
-local EffectTemplate = import('/lua/EffectTemplates.lua')
-local HVFlakWeapon = import('/lua/nomadsweapons.lua').HVFlakWeapon
+local NAMFlakWeapon = import('/lua/nomadsweapons.lua').NAMFlakWeapon
 
 NStructureUnit = AddLights(NStructureUnit)
 
 XNB4204 = Class(NStructureUnit) {
     Weapons = {
-        Turret01 = Class(HVFlakWeapon) {
-
-
-            IdleState = State(HVFlakWeapon.IdleState) { --TODO: refactor this so all the counting is done in the HVFlakWeapon and here you just set parameters
-                Main = function(self)
-                    HVFlakWeapon.IdleState.Main(self)
-                    self.unit:OnTargetLost()
-                    if self.IdleReloadThread then
-                        KillThread(self.IdleReloadThread)
-                    end
-                    self.IdleReloadThread = self:ForkThread(self.ReloadThread)
-                end,
-
-
-                ReloadThread = function(self)
-                    WaitSeconds(1.4)
-                    if not self.PlayingTAEffects then
-                        self.counter = 0
-                    end
-                end,
-            },
-
-            RackSalvoReloadState = State(HVFlakWeapon.RackSalvoReloadState) {
-                Main = function(self)
-                    ForkThread(function()
-                        WaitSeconds(1.4)
-                        if not self or self:BeenDestroyed() then return end
-                        HVFlakWeapon.RackSalvoReloadState.Main(self)
-                        self.unit:OnTargetLost()
-                    end)
-                end,
-            },
-
-            RackSalvoFireReadyState = State(HVFlakWeapon.RackSalvoFireReadyState ) {
-                Main = function(self)
-                    HVFlakWeapon.RackSalvoFireReadyState.Main(self)
-                    self.unit:OnTargetAcquired()
-                end,
-            },
-
-            RackSalvoFiringState = State(HVFlakWeapon.RackSalvoFiringState ) {
-                Main = function(self)
-                    if not self.counter then
-                        self.counter = 0
-                    end
-                    self.counter = self.counter + 1
-                    if self.counter > 5 then
-                        self.counter = 0
-                        self.RackSalvoReloadState.Main(self)
-                    else
-                        self:PlaySound(self.Audio.FireSpecial)
-                        HVFlakWeapon.RackSalvoFiringState.Main(self)
-                        self.unit:OnTargetAcquired()
-                    end
-                end,
-
-                Audio = {
-                    FireSpecial = Sound {
-                        Bank = 'NomadsWeapons',
-                        Cue = 'DarkMatterCannon2_Muzzle',
-                        LodCutoff = 'Weapon_LodCutoff',
-                    },
-                },
-            },
+        Turret01 = Class(NAMFlakWeapon) {
+            TMDEffectBones = {'RadarDish',},
+            SalvoReloadTime = 1.4, --Change this to the correct amount for the weapon.
         },
     },
 
     LightBones = {
         { 'Light1', }, { 'Light2', }, { 'Light3', }, { 'Light4', }, { 'Light5', }, { 'Light6', },
     },
-
-    OnCreate = function(self)
-        NStructureUnit.OnCreate(self)
-        self.TAEffectsBag = TrashBag()
-        self.PlayingTAEffects = false
-    end,
-
-    OnDestroy = function(self)
-        self:DestroyTAEffects()
-        NStructureUnit.OnDestroy(self)
-    end,
-
-    OnTargetAcquired = function(self)
-        --LOG('OnTargetAcquired')
-        self:PlayTAEffects()
-    end,
-
-    OnTargetLost = function(self)
-        --LOG('OnTargetLost')
-        self:DestroyTAEffects()
-    end,
-
-    PlayTAEffects = function(self)
-        if not self.PlayingTAEffects then
-            local bone, army, emit = 'RadarDish', self:GetArmy()
-            for k, v in NomadsEffectTemplate.T2TacticalMissileDefenseTargetAcquired do
-                emit = CreateAttachedEmitter(self, bone, army, v)
-                self.TAEffectsBag:Add(emit)
-                self.Trash:Add(emit)
-            end
-            self.PlayingTAEffects = true
-        end
-    end,
-
-    DestroyTAEffects = function(self)
-        self.TAEffectsBag:Destroy()
-        self.PlayingTAEffects = false
-    end,
 }
 
 TypeClass = XNB4204
