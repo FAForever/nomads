50b8f97fbf3a25cfd8ca0e4341d973dd6ac1ec0e
Exotic-Retard <ddoogedoge@gmail.com>
2019-11-30 15:25:19 +0000

    Fix support factory upgrade hotkeys
    So, the other factions prioritise support factory upgrades to be the
    default hotkey action when they can, and now nomads does this too.

--- nomadhook/lua/keymap/upgradetab.lua
+++ nomadhook/lua/keymap/upgradetab.lua
@@ -3,9 +3,9 @@ do
 local nomadupgrades =
 {
     -- nomads
-    ['xnb0101'] = {'xnb0201', 'znb9501'}, -- LAND-1 = LAND-2-HQ , LAND-2-SUP
-    ['xnb0102'] = {'xnb0202', 'znb9502'}, -- AIR-1 = AIR-2-HQ , AIR-2-SUP
-    ['xnb0103'] = {'xnb0203', 'znb9503'}, -- SEA-1 = SEA-2-HQ , SEA-2-SUP
+    ['xnb0101'] = {'znb9501', 'xnb0201'}, -- LAND-1 = LAND-2-HQ , LAND-2-SUP
+    ['xnb0102'] = {'znb9502', 'xnb0202'}, -- AIR-1 = AIR-2-HQ , AIR-2-SUP
+    ['xnb0103'] = {'znb9503', 'xnb0203'}, -- SEA-1 = SEA-2-HQ , SEA-2-SUP
 
     ['xnb0201'] = 'xnb0301',  -- LAND-2-HQ = LAND-3-HQ
     ['xnb0202'] = 'xnb0302',  -- AIR-2-HQ = AIR-3-HQ
