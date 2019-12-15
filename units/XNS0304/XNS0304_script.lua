e17ee7d46565a27e269210590e7e81c96175003a
Apofenas <Apofenas@gmail.com>
2019-12-15 09:45:33 +0900

    Revert Leviathan rework
    Remove billy nuke weapon
    Buff torp dps
    Nerf ability of torpedoes to break torp defence
    Revert missile range and muzzlesalvosize (emp and missile spread stay)

--- units/XNS0304/XNS0304_script.lua
+++ units/XNS0304/XNS0304_script.lua
@@ -5,7 +5,6 @@ local AddNavalLights = import('/lua/nomadsutils.lua').AddNavalLights
 local NSubUnit = import('/lua/nomadsunits.lua').NSubUnit
 local TorpedoWeapon1 = import('/lua/nomadsweapons.lua').TorpedoWeapon1
 local TacticalMissileWeapon1 = import('/lua/nomadsweapons.lua').TacticalMissileWeapon1
-local StrategicMissileWeapon = import('/lua/nomadsweapons.lua').StrategicMissileWeapon
 
 NSubUnit = AddNavalLights(NSubUnit)
 
@@ -13,17 +12,11 @@ XNS0304 = Class(NSubUnit) {
     Weapons = {
         MissileLauncher1 = Class(TacticalMissileWeapon1) {},
         Torpedo = Class(TorpedoWeapon1) {},
-        TacNukeMissile = Class(StrategicMissileWeapon) {},
     },
 
     DeathThreadDestructionWaitTime = 2,
     LightBone_Left = 'Light1',
     LightBone_Right = 'Light2',
-
-    OnCreate = function(self)
-        self.NukeEntity = 1
-        NSubUnit.OnCreate(self)
-    end,
 }
 
 TypeClass = XNS0304
