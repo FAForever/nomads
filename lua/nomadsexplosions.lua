2109684c114e44ef00b54141647122f2053808cf
speed2 <GTGTGT@seznam.cz>
2020-02-15 10:47:55 +0100

    Cleanup unused imports in nomadsexplosions

--- lua/nomadsexplosions.lua
+++ lua/nomadsexplosions.lua
@@ -1,21 +1,3 @@
-local Entity = import('/lua/sim/entity.lua').Entity
-local EffectTemplate = import('/lua/EffectTemplates.lua')
-local util = import('utilities.lua')
-local GetRandomFloat = util.GetRandomFloat
-local GetRandomInt = util.GetRandomInt
-local GetRandomOffset = util.GetRandomOffset
-local GetRandomOffset2 = util.GetRandomOffset2
-
-local EfctUtil = import('EffectUtilities.lua')
-local CreateEffects = EfctUtil.CreateEffects
-local CreateEffectsWithOffset = EfctUtil.CreateEffectsWithOffset
-local CreateEffectsWithRandomOffset = EfctUtil.CreateEffectsWithRandomOffset
-local CreateBoneEffects = EfctUtil.CreateBoneEffects
-local CreateBoneEffectsOffset = EfctUtil.CreateBoneEffectsOffset
-local CreateRandomEffects = EfctUtil.CreateRandomEffects
-local ScaleEmittersParam = EfctUtil.ScaleEmittersParam
-
-
 local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
 
 --TODO:Remove this?
