0353156faa324cf30a3cc105bff08b154e871e8c
Exotic-Retard <ddoogedoge@gmail.com>
2020-02-23 10:24:57 +0000

    Add nomads notify messages
    This adds notify messages for nomads. an issue is that they way notify
    works, this is mostly useless right now since you need to reset your
    notify from the menu to get the new messages. It would be good to check
    if they are there and then add them in if theyre missing on startup.

---
+++ nomadhook/lua/ui/notify/defaultmessages.lua
@@ -0,0 +1,38 @@
+-- The default messages to broadcast to other players. Loaded into prefs if prefs doesn't have this table
+
+defaultMessages.nomads = {
+    ResourceAllocation = "RAS",
+    ResourceAllocationAdvanced = "ARAS",
+    AdvancedEngineering = "T2",
+    T3Engineering = "T3",
+    DoubleGuns = "Double Gun (Damage)",
+    GunUpgrade = "Gun (AOE&Range)",
+    IntelProbe = "Intel Probe",
+    IntelProbeAdv = "Advanced Intel Probe",
+    MovementSpeedIncrease = "Locomotor",
+    OrbitalBombardment = "Light Orbital Bombardment",
+    OrbitalBombardmentHeavy = "Heavy Orbital Bombardment",
+    PowerArmor = "Power Armor",
+    RapidRepair = "Rapid Repair",
+    Capacitor = "Capacitor",
+}
+    
+defaultMessages.experimentals.xnl0402 = "Beamer"
+defaultMessages.experimentals.xna0401 = "Atlas"
+defaultMessages.experimentals.xnl0403 = "Crawler"
+
+-- This table maps the in-game upgrade names to more colloquial terms so that players can more easily know which message to change
+
+clarityTable.DoubleGuns = "Double Gun Upgrade"
+clarityTable.GunUpgrade = "Gun Upgrade"
+clarityTable.IntelProbe = "Intel Probe"
+clarityTable.IntelProbeAdv = "Advanced Intel Probe"
+clarityTable.MovementSpeedIncrease = "Movement Speed Upgrade"
+clarityTable.OrbitalBombardment = "Light Orbital Bombardment"
+clarityTable.OrbitalBombardmentHeavy = "Heavy Orbital Bombardment"
+clarityTable.PowerArmor = "Power Armor"
+clarityTable.RapidRepair = "Rapid Repair"
+clarityTable.Capacitor = "Capacitor"
+clarityTable.xnl0402 = "Beamer"
+clarityTable.xna0401 = "Atlas"
+clarityTable.xnl0403 = "Crawler"
