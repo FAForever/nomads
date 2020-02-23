7cbdeca82393b83a131ae4795930d8d97eeeb5ce
Exotic-Retard <ddoogedoge@gmail.com>
2020-02-23 12:26:48 +0000

    Remove unused anchor ability from T3 tank
    The function isnt used so no need to import it, this is part of the
    anchor code cleanup.

--- units/XNL0305/XNL0305_Script.lua
+++ units/XNL0305/XNL0305_Script.lua
@@ -1,6 +1,5 @@
 -- T3 tank
 
-local AddAnchorAbilty = import('/lua/nomadsutils.lua').AddAnchorAbilty
 local NLandUnit = import('/lua/nomadsunits.lua').NLandUnit
 local PlasmaCannon = import('/lua/nomadsweapons.lua').PlasmaCannon
 
