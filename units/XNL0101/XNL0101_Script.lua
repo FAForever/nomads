bb23313fb98c22ed12b810039ade5d31b6cee478
Apofenas <Apofenas@gmail.com>
2020-02-23 19:09:54 +0900

    Remove anchor from land scout

--- units/XNL0101/XNL0101_Script.lua
+++ units/XNL0101/XNL0101_Script.lua
@@ -1,6 +1,5 @@
 -- t1 land scout
 
-local AddAnchorAbilty = import('/lua/nomadsutils.lua').AddAnchorAbilty
 local NHoverLandUnit = import('/lua/nomadsunits.lua').NHoverLandUnit
 local DarkMatterWeapon1 = import('/lua/nomadsweapons.lua').DarkMatterWeapon1
 local SlowHover = import('/lua/defaultunits.lua').SlowHoverLandUnit
@@ -11,14 +10,6 @@ XNL0101 = Class(NHoverLandUnit, SlowHover) {
     Weapons = {
         MainGun = Class(DarkMatterWeapon1) {},
     },
-
-    EnableSpecialToggle = function(self)
-        self:EnableAnchor(self)
-    end,
-
-    DisableSpecialToggle = function(self)
-        self:DisableAnchor(self)
-    end,
 }
 
 TypeClass = XNL0101
