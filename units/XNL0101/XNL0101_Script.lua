72b2dcb68226f3eb1fadd6559602c74a1e37561e
Exotic-Retard <ddoogedoge@gmail.com>
2020-02-23 12:25:34 +0000

    Fix T1 Land Scout anchor error in script
    This is due to apofenas removing the anchor but not checking if the
    anchor is used anywhere and so theres this unknown global var. oh well,
    fixed now, so all is right again.

--- units/XNL0101/XNL0101_Script.lua
+++ units/XNL0101/XNL0101_Script.lua
@@ -4,8 +4,6 @@ local NHoverLandUnit = import('/lua/nomadsunits.lua').NHoverLandUnit
 local DarkMatterWeapon1 = import('/lua/nomadsweapons.lua').DarkMatterWeapon1
 local SlowHover = import('/lua/defaultunits.lua').SlowHoverLandUnit
 
-NHoverLandUnit = AddAnchorAbilty(NHoverLandUnit)
-
 XNL0101 = Class(NHoverLandUnit, SlowHover) {
     Weapons = {
         MainGun = Class(DarkMatterWeapon1) {},
