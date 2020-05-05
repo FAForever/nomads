0644c14c058ed2e60657157f1e80a41235aa53fd
Apofenas <Apofenas@gmail.com>
2020-05-06 04:09:03 +0900

    Fix: TML
    Use different missile projectile because old one is used my MML
    (surprise).

--- projectiles/NTacticalMissile1_HighArc/NTacticalMissile1_HighArc_Script.lua
+++ projectiles/NTacticalMissile1_HighArc/NTacticalMissile1_HighArc_Script.lua
@@ -1,26 +1,49 @@
-local ArcingTacticalMissile = import('/lua/nomadsprojectiles.lua').ArcingTacticalMissile
+local NIFCruiseMissile = import('/lua/nomadsprojectiles.lua').NIFCruiseMissile
+local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 
-NTacticalMissile1_HighArc = Class(ArcingTacticalMissile) {
-	OnCreate = function(self, inWater)
-        ArcingTacticalMissile.OnCreate(self, inWater)
+NTacticalMissile1 = Class(NIFCruiseMissile) {
+    FxImpactAirUnit = NomadsEffectTemplate.TacticalMissileHitAirUnit2,
+    FxImpactLand = NomadsEffectTemplate.TacticalMissileHitLand2,
+    FxImpactNone = NomadsEffectTemplate.TacticalMissileHitNone2,
+    FxImpactProp = NomadsEffectTemplate.TacticalMissileHitProp2,
+    FxImpactShield = NomadsEffectTemplate.TacticalMissileHitShield2,
+    FxImpactUnit = NomadsEffectTemplate.TacticalMissileHitUnit2,
+    FxImpactWater = NomadsEffectTemplate.TacticalMissileHitWater2,
+    FxImpactProjectile = NomadsEffectTemplate.TacticalMissileHitProjectile2,
+    FxImpactUnderWater = NomadsEffectTemplate.TacticalMissileHitUnderWater2,
 
-        self:SetTurnRate(0)
-        self:ChangeMaxZigZag(0)
-        self:ChangeZigZagFrequency(0)
-        self:ForkThread(self.StageThread)
+    MovementThread = function(self)
+        self.WaitTime = 0.1
+        if self:GetDistanceToTarget() <= 10 then
+            self:SetTurnRate(180)
+        else
+            self:SetTurnRate(10)
+        end
+        WaitSeconds(0.2)
+        while not self:BeenDestroyed() do
+            self:SetTurnRateByDist()
+            WaitSeconds(self.WaitTime)
+        end
     end,
 
-    StageThread = function(self)
-        WaitSeconds(4)
-        local bp = self:GetBlueprint().Physics
-        self:SetTurnRate(bp.TurnRate)
-        self:ChangeMaxZigZag(bp.MaxZigZag)
-        self:ChangeZigZagFrequency(bp.ZigZagFrequency)
-        WaitSeconds(1)
-        self:ChangeMaxZigZag(0)
-        self:ChangeZigZagFrequency(0)
+    SetTurnRateByDist = function(self)
+        local dist = self:GetDistanceToTarget() --defined in NIFCruiseMissile class
+        if dist > 70 then
+            self:SetTurnRate(9)
+        elseif dist > 50 then
+            self:SetTurnRate(25)
+        elseif dist > 40 and dist <= 213 then
+            self:SetTurnRate(20)
+            WaitSeconds(0.5)
+            self:SetTurnRate(30)
+        elseif dist > 20 and dist <= 40 then
+            WaitSeconds(0.3)
+            self:SetTurnRate(50)
+        elseif dist > 0 and dist <= 20 then
+            self:SetTurnRate(100)
+            KillThread(self.MoveThread)
+        end
     end,
-
 }
 
-TypeClass = NTacticalMissile1_HighArc
+TypeClass = NTacticalMissile1
