f9971514fd951f38f49d4bc6564f2eccb78578b5
Exotic-Retard <ddoogedoge@gmail.com>
2020-06-27 17:41:04 +0100

    Rework Crawler missiles
    They were traveling quite fast so sometimes Cybran TMD would miss them!
    So they have a larger collision box as a result, which solves the issue.
    Additionally they are now part of the new NIFMissile class which has the
    unpacking function, and also have a slightly different turning rate
    pattern.

--- projectiles/NTacticalMissile2/NTacticalMissile2_Script.lua
+++ projectiles/NTacticalMissile2/NTacticalMissile2_Script.lua
@@ -1,10 +1,11 @@
-local TacticalMissile = import('/lua/nomadsprojectiles.lua').TacticalMissile
+local NIFCruiseMissile = import('/lua/nomadsprojectiles.lua').NIFCruiseMissile
 local RandomOffsetTrackingTarget = import('/lua/utilities.lua').RandomOffsetTrackingTarget
 
-NTacticalMissile2 = Class(TacticalMissile) {
+NTacticalMissile2 = Class(NIFCruiseMissile) {
 	
 	OnCreate = function(self, inWater)
-        TacticalMissile.OnCreate(self, inWater)
+        NIFCruiseMissile.OnCreate(self, inWater)
+        self:SetCollisionShape('Sphere', 0, 0, 0, 3.0) --these travel at a high speed so cybran which uses beams can miss!
         self:SetTurnRate(0)
         self:ForkThread(self.StageThread)
     end,
@@ -14,6 +15,23 @@ NTacticalMissile2 = Class(TacticalMissile) {
         self:SetTurnRate(self:GetBlueprint().Physics.TurnRate)
 		
         self:SetNewTargetGround(RandomOffsetTrackingTarget(self, 10))
+    end,
+    
+    SetTurnRateByDist = function(self)
+        local dist = self:GetDistanceToTarget()
+        if dist > 50 then
+            self:SetTurnRate(50)
+        elseif dist > 40 and dist <= 213 then
+            self:SetTurnRate(40)
+            WaitSeconds(0.5)
+            self:SetTurnRate(60)
+        elseif dist > 20 and dist <= 40 then
+            WaitSeconds(0.3)
+            self:SetTurnRate(100)
+        elseif dist > 0 and dist <= 20 then
+            self:SetTurnRate(100)
+            KillThread(self.MoveThread)
+        end
     end,
 }
 
