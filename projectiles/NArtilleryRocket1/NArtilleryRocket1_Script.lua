a3a70cfa9ee31c780659e17f8a335eeaf0cda11a
Apofenas <Apofenas@gmail.com>
2020-07-11 13:16:56 +0900

    Nerf Barrager
    Huge nerf of Barrager projectile speed
    Damage radius nerf 1->0.5
    DPS nerf 80->70
    ROF 1->0.6
    Damage 80->130

--- projectiles/NArtilleryRocket1/NArtilleryRocket1_Script.lua
+++ projectiles/NArtilleryRocket1/NArtilleryRocket1_Script.lua
@@ -1,37 +1,41 @@
 local Rocket1 = import('/lua/nomadsprojectiles.lua').Rocket1
 
 NArtilleryRocket1 = Class(Rocket1) {
-	OnCreate = function(self)
-		Rocket1.OnCreate(self)
+    OnCreate = function(self)
+        Rocket1.OnCreate(self)
         self:SetTurnRate(0)
         self:TrackTarget(true)
         self:ForkThread(self.MovementThread) 
     end,
-	
-	MovementThread = function(self)
+    
+    MovementThread = function(self)
+        local bp = self:GetBlueprint().Physics
         self.WaitTime = 0.1
         self:SetTurnRate(8)
-        WaitSeconds(0.3)    
+        WaitSeconds(0.3)
         while not self:BeenDestroyed() do
             self:SetTurnRateByDist()
             WaitSeconds(1)
         end
     end,
-	
-	SetTurnRateByDist = function(self)
+    
+    SetTurnRateByDist = function(self)
         local dist = self:GetDistanceToTarget()
-		local bp = self:GetBlueprint().Physics
         if dist > 20 then        
             WaitSeconds(0.5)
             self:SetTurnRate(175)
+            self:SetMaxSpeed(15)
         elseif dist > 10 and dist <= 20 then
             WaitSeconds(0.5)
             self:SetTurnRate(200)
+            self:SetMaxSpeed(12)
         elseif dist > 5 and dist <= 10 then
             WaitSeconds(0.1)
             self:SetTurnRate(225)
+            self:SetMaxSpeed(9)
         elseif dist > 0 and dist <= 5 then          
-            self:SetTurnRate(250)   
+            self:SetTurnRate(250)
+            self:SetMaxSpeed(6)
             KillThread(self.MoveThread)         
         end
     end,        
