5592a5180b74cc402b2f0e069b551284630c8eed
Apofenas <Apofenas@gmail.com>
2020-08-20 13:20:32 +0900

    Nerf Mauler
    Remove meta of arty spam for nomads. This change doubles damage per
    shot, but also doubles reload time (15 sec now).
    Arty is more accurate when stationary, but much less accurate while
    moving.

--- units/XNL0304/XNL0304_Script.lua
+++ units/XNL0304/XNL0304_Script.lua
@@ -7,11 +7,31 @@ local EffectUtil = import('/lua/EffectUtilities.lua')
 
 XNL0304 = Class(NLandUnit) {
     Weapons = {
-		ArtilleryGun = Class(ArtilleryWeapon) {},
+        ArtilleryGun = Class(ArtilleryWeapon) {
+            SetMovingAccuracy = function(self, bool)
+                local bp = self:GetBlueprint()
+                if bool then
+                    self:SetFiringRandomness( bp.FiringRandomnessWhileMoving or (math.max(0, bp.FiringRandomness) * 4) )
+                else
+                    self:SetFiringRandomness( bp.FiringRandomness )
+                end
+            end,
+        },
     },
-	
-	OnCreate = function(self)
+    
+    OnCreate = function(self)
         NLandUnit.OnCreate(self)
+    end,
+        
+    OnMotionHorzEventChange = function( self, new, old )
+        NLandUnit.OnMotionHorzEventChange( self, new, old )
+        self:UpdateWeaponAccuracy( (new ~= 'Stopped' and new ~= 'Stopping') )
+    end,
+
+    UpdateWeaponAccuracy = function(self, moving)
+        if not self.Dead then
+            self:GetWeapon(1):SetMovingAccuracy(moving)
+        end
     end,
 }
 
