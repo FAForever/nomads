fec1a1e9e43dd056021bc2e966da09b68d21cf63
Steven Cripe <stevenc21@protonmail.com>
2020-08-21 13:38:09 -0700

    Preliminary Change

--- projectiles/NIFOrbitalMissile01/NIFOrbitalMissile01_Script.lua
+++ projectiles/NIFOrbitalMissile01/NIFOrbitalMissile01_Script.lua
@@ -5,3 +5,15 @@ NIFOrbitalMissile01 = Class(NIFOrbitalMissile) {
 }
 
 TypeClass = NIFOrbitalMissile01
+
+FxAirUnitHitScale = 0.95,
+FxLandHitScale = 0.95,
+FxNoneHitScale = 0.95,
+FxPropHitScale = 0.95,
+FxProjectileHitScale = 0.95,
+FxProjectileUnderWaterHitScale = 0.95,
+FxShieldHitScale = 0.95,
+FxUnderWaterHitScale = 0.95 * 0.25,
+FxUnitHitScale = 0.95,
+FxWaterHitScale = 0.95,
+FxOnKilledScale = 0.95,
