f31443eada8004e80bd03d3862007607ff0adaf0
Steven Cripe <stevenc21@protonmail.com>
2020-08-21 14:30:38 -0700

    Reformat + Shrink Explosion
    Created a new Tactical Missile projectile for the T4 MML, to make the explosion smaller.
    
    Also a quick reformat.

--- projectiles/NIFOrbitalMissile01/NIFOrbitalMissile01_Script.lua
+++ projectiles/NIFOrbitalMissile01/NIFOrbitalMissile01_Script.lua
@@ -2,18 +2,19 @@ local NIFOrbitalMissile = import('/lua/nomadsprojectiles.lua').NIFOrbitalMissile
 
 NIFOrbitalMissile01 = Class(NIFOrbitalMissile) {
     TargetSpread = 10,--This controls the spread of the bombardment projectiles
+	FxAirUnitHitScale = 0.95,
+	FxLandHitScale = 0.95,
+	FxNoneHitScale = 0.95,
+	FxPropHitScale = 0.95,
+	FxProjectileHitScale = 0.95,
+	FxProjectileUnderWaterHitScale = 0.95,
+	FxShieldHitScale = 0.95,
+	FxUnderWaterHitScale = 0.95 * 0.25,
+	FxUnitHitScale = 0.95,
+	FxWaterHitScale = 0.95,
+	FxOnKilledScale = 0.95,
+
 }
 
 TypeClass = NIFOrbitalMissile01
 
-FxAirUnitHitScale = 0.95,
-FxLandHitScale = 0.95,
-FxNoneHitScale = 0.95,
-FxPropHitScale = 0.95,
-FxProjectileHitScale = 0.95,
-FxProjectileUnderWaterHitScale = 0.95,
-FxShieldHitScale = 0.95,
-FxUnderWaterHitScale = 0.95 * 0.25,
-FxUnitHitScale = 0.95,
-FxWaterHitScale = 0.95,
-FxOnKilledScale = 0.95,
