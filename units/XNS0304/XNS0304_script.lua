f193f94d7b77b7f28eef87d23e20c68a6a7b2bbb
Apofenas <Apofenas@gmail.com>
2020-08-22 18:00:42 +0900

    Various fixes to Leviathan missiles
    Leviathan now gets MaxRadius buff when unmerged
    Missiles now gain height before heading towards target
    Collision Shape is increased to make easier for TMD to shoot down
    missiles
    Projectile size decreased to match unit model
    Some fixes with water impact.

--- units/XNS0304/XNS0304_script.lua
+++ units/XNS0304/XNS0304_script.lua
@@ -17,6 +17,45 @@ XNS0304 = Class(NSubUnit) {
     DeathThreadDestructionWaitTime = 2,
     LightBone_Left = 'Light1',
     LightBone_Right = 'Light2',
+	
+    OnCreate = function(self)
+        -- create buff used to reduce range under water
+        local wep = self:GetWeaponByLabel('MissileLauncher1')
+        if wep then
+            self.OnInWaterBuff = 'XNS0304_OnWater'
+            local wbp = wep:GetBlueprint()
+            local MaxRadiusUnderWater = wbp.MaxRadiusUnderWater or wbp.MaxRadius
+            BuffBlueprint {
+                Name = self.OnInWaterBuff,
+                DisplayName = self.OnInWaterBuff,
+                BuffType = string.upper(self.OnInWaterBuff),
+                Stacks = 'REPLACE',
+                Duration = -1,
+                Affects = {
+                    MaxRadiusSpecifiedWeapons = {
+                        Add = (MaxRadiusUnderWater - wbp.MaxRadius),
+                    },
+                },
+            }
+        else
+            self.OnInWaterBuff = nil
+        end
+
+        NSubUnit.OnCreate(self)
+    end,
+	
+    OnLayerChange = function(self, new, old)
+        NSubUnit.OnLayerChange(self, new, old)
+        if new == 'Water' then
+            if self.OnInWaterBuff then
+                Buff.RemoveBuff(self, self.OnInWaterBuff, true)
+            end
+        elseif new == 'Sub' or new == 'Seabed' then
+            if self.OnInWaterBuff then
+                Buff.ApplyBuff(self, self.OnInWaterBuff)
+            end
+        end
+    end,
 }
 
 TypeClass = XNS0304
