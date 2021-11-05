c65bfe79fddd7c2d1e7570d103a9ceded16e40e2
McChaffeeFaF <mcchaffee943@gmail.com>
2021-11-04 22:02:33 -0400

    Particle Fixes for T1 Gunship/LAB

--- units/XNL0106/XNL0106_Script.lua
+++ units/XNL0106/XNL0106_Script.lua
@@ -1,12 +1,12 @@
 -- T1 light tank (should match LABs in power)
 
 local NHoverLandUnit = import('/lua/nomadsunits.lua').NHoverLandUnit
-local DarkMatterWeapon1 = import('/lua/nomadsweapons.lua').DarkMatterWeapon1
+local ParticleBlaster1 = import('/lua/nomadsweapons.lua').ParticleBlaster1
 local SlowHover = import('/lua/defaultunits.lua').SlowHoverLandUnit
 
 XNL0106 = Class(NHoverLandUnit, SlowHover) {
     Weapons = {
-        MainGun = Class(DarkMatterWeapon1) {},
+        MainGun = Class(ParticleBlaster1) {},
     },
 }
 
