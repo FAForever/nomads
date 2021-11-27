75486e8340f456e869f74a5e75e60a3c1b172957
Apofenas <Apofenas@gmail.com>
2021-11-27 12:43:00 +0900

    3725:Silhouette
    Adjust stats to FAF balance changes;
    Disabled cosmetic weapon to prevent any attempt to go away from guarded unit;
    Added sonar as a side ability.

--- units/XNL0101/XNL0101_Script.lua
+++ units/XNL0101/XNL0101_Script.lua
@@ -1,13 +1,9 @@
 -- t1 land scout
 
 local NHoverLandUnit = import('/lua/nomadsunits.lua').NHoverLandUnit
-local DarkMatterWeapon1 = import('/lua/nomadsweapons.lua').DarkMatterWeapon1
 local SlowHover = import('/lua/defaultunits.lua').SlowHoverLandUnit
 
 XNL0101 = Class(NHoverLandUnit, SlowHover) {
-    Weapons = {
-        MainGun = Class(DarkMatterWeapon1) {},
-    },
 }
 
 TypeClass = XNL0101
