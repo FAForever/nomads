d242188cc0909badc8b92eb4befb46573f23ecf3
Exotic-Retard <ddoogedoge@gmail.com>
2022-11-07 13:25:17 +0000

    Fix issue with upgraded buildings breaking
    This was caused by a change in format for the upgrade tab requirements
    which necessitated a table in the latest faf patch.
    
    As such the format in the upgrade tab is now updated - where single
    string values are now removed as they are read from the unit blueprints.

--- nomadhook/lua/keymap/upgradetab.lua
+++ nomadhook/lua/keymap/upgradetab.lua
@@ -6,21 +6,6 @@ local nomadupgrades =
     ['xnb0101'] = {'znb9501', 'xnb0201'}, -- LAND-1 = LAND-2-HQ , LAND-2-SUP
     ['xnb0102'] = {'znb9502', 'xnb0202'}, -- AIR-1 = AIR-2-HQ , AIR-2-SUP
     ['xnb0103'] = {'znb9503', 'xnb0203'}, -- SEA-1 = SEA-2-HQ , SEA-2-SUP
-
-    ['xnb0201'] = 'xnb0301',  -- LAND-2-HQ = LAND-3-HQ
-    ['xnb0202'] = 'xnb0302',  -- AIR-2-HQ = AIR-3-HQ
-    ['xnb0203'] = 'xnb0303',  -- SEA-2-HQ = SEA-3-HQ
-
-    ['znb9501']  = 'znb9601',  -- LAND-2-SUP = LAND-3-SUP
-    ['znb9502']  = 'znb9602',  -- AIR-2-SUP = AIR-3-SUP
-    ['znb9503']  = 'znb9603',  -- SEA-2-SUP = SEA-3-SUP
-
-    ['xnb3102']  = 'xnb3202',   -- sonar
-    ['xnb3202']  = 'xnb3302',
-    ['xnb3101']  = 'xnb3201',   -- radar
-    ['xnb3201']  = 'xnb3301',
-    ['xnb1103']  = 'xnb1202',   -- mex
-    ['xnb1202']  = 'xnb1302',
 }
 
 upgradeTab = table.merged(upgradeTab, nomadupgrades)
