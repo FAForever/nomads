89dc60d582f6ca02ba711bfbe5eebc7003caffeb
Apofenas <FedotovAlexeyM@yandex.ru>
2024-01-05 12:27:16 +0900

    3761: Fix Crawler missiles StageThread
    No idea why exactly it stopped working, but now it works.

--- projectiles/NTacticalMissile2/NTacticalMissile2_Script.lua
+++ projectiles/NTacticalMissile2/NTacticalMissile2_Script.lua
@@ -9,11 +9,12 @@ NTacticalMissile2 = Class(NIFCruiseMissile) {
         self.TargetPos = RandomOffsetTrackingTarget(self, 10)
         self:SetCollisionShape('Sphere', 0, 0, 0, 3.0) --these travel at a high speed so cybran which uses beams can miss!
         self:SetTurnRate(0)
-        self:SetNewTargetGround({pos[1],pos[2] + 70,pos[3]})
         self:ForkThread(self.StageThread)
     end,
 
     StageThread = function(self)
+        local pos = self:GetPosition()
+        self:SetNewTargetGround({pos[1],pos[2] + 70,pos[3]})
         WaitSeconds(1 +Random(0,100)*0.01)
         self:SetNewTargetGround({self.TargetPos[1],self.TargetPos[2],self.TargetPos[3]})
         self:SetTurnRateByDist()
