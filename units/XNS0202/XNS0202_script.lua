b0d2c5d2ffbf4fa46b8590d727aa7130d500f57c
Apofenas <FedotovAlexeyM@yandex.ru>
2024-01-08 16:17:22 +0900

    3761: Mercenary
    Remove EMP weapon from Cruiser

--- units/XNS0202/XNS0202_script.lua
+++ units/XNS0202/XNS0202_script.lua
@@ -3,22 +3,14 @@
 local AddNavalLights = import('/lua/nomadsutils.lua').AddNavalLights
 local NSeaUnit = import('/lua/nomadsunits.lua').NSeaUnit
 local RocketWeapon1 = import('/lua/nomadsweapons.lua').RocketWeapon1
-local EMPGun = import('/lua/nomadsweapons.lua').EMPGun
+local EnergyCannon1 = import('/lua/nomadsweapons.lua').EnergyCannon1
 
 NSeaUnit = AddNavalLights(NSeaUnit)
 
 XNS0202 = Class(NSeaUnit) {
     Weapons = {
         AATurret = Class(RocketWeapon1) {},
-        GunTurret = Class(EMPGun) {
-            CreateProjectileAtMuzzle = function(self, muzzle)
-                local proj = EMPGun.CreateProjectileAtMuzzle(self, muzzle)
-                local data = self:GetBlueprint().DamageToShields
-                if proj and not proj:BeenDestroyed() then
-                    proj:PassData(data)
-                end
-            end,
-        },
+        GunTurret = Class(EnergyCannon1) {},
     },
 
     LightBone_Left = 'Light2',
