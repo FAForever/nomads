8e667210143d79876718a93da56ea7788883fb23
Apofenas <FedotovAlexeyM@yandex.ru>
2024-01-09 19:52:09 +0900

    3761: Turn Arbalast into Nomad t3 static arty

--- projectiles/NTacticalMissile3_HighArc/NTacticalMissile3_HighArc_Script.lua
+++ projectiles/NTacticalMissile3_HighArc/NTacticalMissile3_HighArc_Script.lua
@@ -1,59 +1,41 @@
-local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
-local ArcingTacticalMissile = import('/lua/nomadsprojectiles.lua').ArcingTacticalMissile
-local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
-local util = import('/lua/utilities.lua')
-local GetRandomFloat = util.GetRandomFloat
+local NIFArtilleryMissile = import('/lua/nomadsprojectiles.lua').NIFArtilleryMissile
+local RandomOffsetTrackingTarget = import('/lua/utilities.lua').RandomOffsetTrackingTarget
 
-NTacticalMissile3_HighArc = Class(ArcingTacticalMissile) {
-    FxImpactAirUnit = NomadsEffectTemplate.ArcingTacticalMissileHitAirUnit2,
-    FxImpactLand = NomadsEffectTemplate.ArcingTacticalMissileHitLand2,
-    FxImpactNone = NomadsEffectTemplate.ArcingTacticalMissileHitNone2,
-    FxImpactProp = NomadsEffectTemplate.ArcingTacticalMissileHitProp2,
-    FxImpactShield = NomadsEffectTemplate.ArcingTacticalMissileHitShield2,
-    FxImpactUnit = NomadsEffectTemplate.ArcingTacticalMissileHitUnit2,
-    FxImpactWater = NomadsEffectTemplate.ArcingTacticalMissileHitWater2,
-    FxImpactProjectile = NomadsEffectTemplate.ArcingTacticalMissileHitProjectile2,
-    FxImpactUnderWater = NomadsEffectTemplate.ArcingTacticalMissileHitUnderWater2,
-
-    -- To accomodate the Aeon TMD that uses flares to distract and redirect tactical missiles some additional code is added here.
-    -- It makes missiles not speed up if flared by the Aeon TMD but missiles that have sped up do not react to the TMD (this is
-    -- handled in the defaultantimissile.lua file).
-
-    OnImpact = function(self, targetType, targetEntity)
-        if targetType == 'Air' and not targetEntity then
-            if self:IsFlared() then
-                self:ChangeDetonateBelowHeight(0)
-            else
-                self:SpeedUp()
-            end
-        else
-            ArcingTacticalMissile.OnImpact(self, targetType, targetEntity)
-        end
+NTacticalMissile3 = Class(NIFArtilleryMissile) {
+    
+    OnCreate = function(self, inWater)
+        local pos = self:GetPosition()
+        NIFArtilleryMissile.OnCreate(self, inWater)
+        self.TargetPos = RandomOffsetTrackingTarget(self, 20)
+	    self:SetTurnRate(0)
+        self:ForkThread(self.StageThread)
     end,
 
-    SpeedUp = function(self)
-        -- create clone projectile, the original can't increase speed for some reason but a clone can
---        local ChildProjectileBP = '/projectiles/NTacticalMissile3_HighArc/NTacticalMissile3_HighArc_proj.bp'
-        local ChildProjectileBP = '/projectiles/NPlasmaProj1_arty/NPlasmaProj1_arty_proj.bp'
-        local vx, vy, vz = self:GetVelocity()
-        local velocity = self:GetCurrentSpeed() * 30
-
-        local child = self:CreateChildProjectile(ChildProjectileBP)
-        child:ChangeDetonateBelowHeight(0)
-        child:SetMaxSpeed(velocity)
-        child:SetVelocity(vx, vy, vz)
-        child:SetVelocity(velocity)
-        child:PassDamageData(self.DamageData)
-        child:SetLifetime( child:GetBlueprint().Physics.LifetimeSpeedup or child:GetBlueprint().Physics.Lifetime or 1 )
-        child.OnFlare_SetTrackTarget = false
-
-        -- Split effects
-        for k, v in NomadsEffectTemplate.ArcingTacticalMissileSpeedupFlash do
-            CreateEmitterAtEntity( self, self.Army, v )
+    StageThread = function(self)
+        WaitSeconds(10)
+        local pos = self:GetPosition()
+        self:SetTurnRateByDist()
+    end,
+    
+    SetTurnRateByDist = function(self)
+	    local dist = self:GetDistanceToTarget()
+        local pos = self:GetPosition()
+        if dist > 600 then
+            self:SetTurnRate(2)
+        elseif dist > 450 and dist <= 600 then
+            self:SetTurnRate(4)
+        elseif dist > 300 and dist <= 450 then
+            self:SetTurnRate(8)
+        elseif dist > 100 and dist <= 300 then
+            self:SetTurnRate(10)
+			self:SetNewTargetGround({self.TargetPos[1],self.TargetPos[2],self.TargetPos[3]})
+        elseif dist > 50 and dist <= 100 then
+            self:SetTurnRate(30)
+        elseif dist > 0 and dist <= 50 then
+            self:SetTurnRate(50) --TODO:Speedup or turn into child projectile
+            KillThread(self.MoveThread)
         end
-
-        self:Destroy()
     end,
 }
 
-TypeClass = NTacticalMissile3_HighArc
+TypeClass = NTacticalMissile3
